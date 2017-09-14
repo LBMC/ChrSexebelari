5b5ead35a5338533f20f2a13bc251ce9a9f94d49
cburny <claire.burny@ens-lyon.fr>
2017-09-14 13:50:20 +0200

    QC_Mapped_Mbelari_fastq.sh: fastqc on local, change --paired false

--- src/QC_Mapped_Mbelari_fastq.sh
+++ src/QC_Mapped_Mbelari_fastq.sh
@@ -3,6 +3,8 @@
 #$ -o /home/cburny/logs
 #$ -e /home/cburny/logs
 
-module load nextflow/0.25.1
+#module load nextflow/0.25.1
 
-nextflow src/pipe/quality_control.nf -c src/nextflow.config -profile quality_control_sge --fastq_files "results/mapping/mapped/*.fastq.gz"
+#nextflow src/pipe/quality_control.nf -c src/nextflow.config -profile quality_control_sge --fastq_files "results/mapping/mapped/*.fastq.gz"
+
+bin/nextflow src/pipe/quality_control.nf -c src/nextflow.config -profile quality_control --fastq_files "results/mapping/mapped/*_trim_Mbelari_mapped_sort.fastq" --cpu 4 --paired false --trimmer "cutadapt" --do_adapter_removal false
