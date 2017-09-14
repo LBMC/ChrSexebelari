e19e820020159849cd9b75f7bfea00f15d9c1b8e
cburny <claire.burny@ens-lyon.fr>
2017-09-14 14:24:04 +0200

    summary-vcf_file.sh: summary vcf file

---
+++ src/summary-vcf_file.sh
@@ -0,0 +1,6 @@
+# vcftools obtained from https://github.com/vcftools/vcftools
+# do not forget to add: export PERL5LIB=/path/to/your/vcftools-directory/src/perl/
+
+/home/cburny/vcftools/src/perl/vcf-stats $1 > $2 summary_raw_var_male_JU2917.txt
+
+
