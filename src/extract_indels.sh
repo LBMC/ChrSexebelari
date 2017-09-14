36efc0472db180a3b58e04a4a6252ab131db92b2
cburny <claire.burny@ens-lyon.fr>
2017-09-14 18:32:19 +0200

    extract_indels.sh: extract INDELs from vcf

---
+++ src/extract_indels.sh
@@ -0,0 +1,5 @@
+/home/cburny/vcftools/src/cpp/vcftools --vcf results/call_var/2017_09_13_MRDR5_trim_Mbelari_mapped_sort_rmdup.vcf --keep-only-indels --out results/call_var/2017_09_14_MRDR5_trim_Mbelari_mapped_sort_rmdup_indels.vcf &&\
+
+/home/cburny/vcftools/src/cpp/vcftools --vcf results/call_var/2017_09_13_MRDR5_trim_Mbelari_mapped_sort_rmdup.vcf --keep-only-indels --out results/call_var/2017_09_14_MRDR5_trim_Mbelari_mapped_sort_rmdup_indels.vcf &&\
+
+
