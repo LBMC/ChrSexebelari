67a6727fc8a5917238bad7d0986c2ce930a962c4
cburny <claire.burny@ens-lyon.fr>
2017-09-15 17:46:07 +0200

    summary-vcf_file.sh: make summary of vcf using rtg tools

--- src/summary-vcf_file.sh
+++ src/summary-vcf_file.sh
@@ -1,6 +1,5 @@
-# vcftools obtained from https://github.com/vcftools/vcftools
-# do not forget to add: export PERL5LIB=/path/to/your/vcftools-directory/src/perl/
+bin/rtg vcfstats results/call_var/2017_09_13_MRDR5_trim_Mbelari_mapped_sort_rmdup.vcf > results/call_var/summary_raw_var_female_JU2917.txt &&\
 
-/home/cburny/vcftools/src/perl/vcf-stats $1 > $2 summary_raw_var_male_JU2917.txt
+bin/rtg vcfstats results/call_var/2017_09_13_MRDR6_trim_Mbelari_mapped_sort_rmdup.vcf > results/call_var/summary_raw_var_male_JU2917.txt
 
 
