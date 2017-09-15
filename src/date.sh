fff34715bb2ed85d0458d64a738b86090169bbd5
cburny <claire.burny@ens-lyon.fr>
2017-09-15 17:48:35 +0200

    date.sh: provide file instead of dir to date file

--- src/date.sh
+++ src/date.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
 
-# This script renames files in a given directory using git repository file_handle. 
+# This script renames file using file_handle. 
 # bash date.sh "dir"
 
-PATH_FILES=$1
-FILES="$(ls $PATH_FILES)"
-
-for f in ${FILES} 
-do
-	python3 ./src/file_handle/src/file_handle.py -f $PATH_FILES/$f
-done
+python3 ./src/file_handle/src/file_handle.py -f $1
