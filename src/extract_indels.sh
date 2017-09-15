9e416631e123ef5c383da7673d86716250208b3b
cburny <claire.burny@ens-lyon.fr>
2017-09-15 11:23:28 +0200

    extract_indels: vcftools function to subset INDELs from VCF

--- src/extract_indels.sh
+++ src/extract_indels.sh
@@ -1,5 +1,3 @@
-/home/cburny/vcftools/src/cpp/vcftools --vcf results/call_var/2017_09_13_MRDR5_trim_Mbelari_mapped_sort_rmdup.vcf --keep-only-indels --out results/call_var/2017_09_14_MRDR5_trim_Mbelari_mapped_sort_rmdup_indels.vcf &&\
+/home/cburny/vcftools/src/cpp/vcftools --vcf results/call_var/2017_09_13_MRDR5_trim_Mbelari_mapped_sort_rmdup.vcf --keep-only-indels > results/call_var/2017_09_14_MRDR5_trim_Mbelari_mapped_sort_rmdup_indels.vcf &&\
 
-/home/cburny/vcftools/src/cpp/vcftools --vcf results/call_var/2017_09_13_MRDR5_trim_Mbelari_mapped_sort_rmdup.vcf --keep-only-indels --out results/call_var/2017_09_14_MRDR5_trim_Mbelari_mapped_sort_rmdup_indels.vcf &&\
-
-
+/home/cburny/vcftools/src/cpp/vcftools --vcf results/call_var/2017_09_13_MRDR5_trim_Mbelari_mapped_sort_rmdup.vcf --keep-only-indels > results/call_var/2017_09_14_MRDR5_trim_Mbelari_mapped_sort_rmdup_indels.vcf
