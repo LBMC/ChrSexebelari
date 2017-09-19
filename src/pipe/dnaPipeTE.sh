50c6236811d615af0e4fc863cb69d8d1096f24fd
Laurent Modolo <laurent.modolo@ens-lyon.fr>
2017-09-19 10:29:25 +0200

    dnaPipeTE.sh: analysis for M and F data

--- src/pipe/dnaPipeTE.sh
+++ src/pipe/dnaPipeTE.sh
@@ -3,17 +3,17 @@
 # female_JU2817
 mkdir -p results/dnaPipeTE/female_JU2817
 python3 src/dnaPipeTE/dnaPipeTE.py \
--input data/fastq/2017_09_13_female_JU2817_R1.fastq.gz \
+-input results/mapping/mapped/2017_09_13_MRDR5_trim_Mbelari_mapped_sort.fastq.gz \
 -output results/dnaPipeTE/female_JU2817 \
 -cpu 12 \
 -genome_size 162071050 \
 -genome_coverage 22.9 \
--sample_number 5
+-sample_number 3
 
 # male_JU2817
 mkdir -p results/dnaPipeTE/male_JU2817
 python3 src/dnaPipeTE/dnaPipeTE.py \
--input data/fastq/2017_09_13_male_JU2817_R1.fastq.gz \
+-input results/mapping/mapped/2017_09_13_male_JU2817_R1.fastq.gz \
 -output results/dnaPipeTE/male_JU2817 \
 -cpu 12 \
 -genome_size 162071050 \
