bf5137a8c1a0d28982959b7a3017a6bd36c85877
cburny <claire.burny@ens-lyon.fr>
2017-09-21 09:53:21 +0200

    picard_dictionnary.sh: create dictionnary from picard locally

--- src/picard_dictionnary.sh
+++ src/picard_dictionnary.sh
@@ -1,34 +1,17 @@
-#!/bin/bash
-### shell du job:
-#$ -S /bin/bash
-### nom du job:
-#$ -N PICARD_dict
-### file d'attente:
-#$ -q E5-2670deb128*
-### parallel environnement & nslots
-#$ -pe mpi16_debian 16 
-### charger l'environnement utilisateur pour SGE
-#$ -cwd
-### exporte les variables d'environnement sur les noeuds d'exécution
-#$ -V
-### mails en debut et fin d execution
-#$ -m be
-### change logs folder
-#$ -o /home/cburny/logs
-#$ -e /home/cburny/logs
+# Create sequence dictionnary
+# Done locally due to problems with PSMN, using picard 2.12.1. picard.jar
+# Obtained on 20 sept 2017: 
+# git clone https://github.com/broadinstitute/picard.git
+#    cd picard/
+#    ./gradlew shadowJar
+# copy build/libs/picard.jar to bin
 
-# initialiser environnement Module
-source /usr/share/modules/init/bash
-module use /applis/PSMN/Modules
-module load Base/psmn 
-module load picard/2.3.0 
-module list
-umask 002
+##### Args #####
+# $1: input ref in fasta
+# $2: output dict 
 
-PICARDHOME="/applis/PSMN/generic/picard/2.3.0"
-REF="/scratch/cburny/Ref_Mbelari/Mesorhabditis_belari_JU2817_v2_scaffolds_repeatmasker_masked.fa"
-REFOUT="/scratch/cburny/Ref_Mbelari/Mesorhabditis_belari_JU2817_v2_scaffolds_repeatmasker_masked.dict"
+java -jar bin/picard.jar CreateSequenceDictionary R=$1 O=$2 &&\
 
-java -jar $PICARDHOME/picard.jar CreateSequenceDictionary R=$REF O= $REFOUT
+samtools faidx $1
 
-echo "termine"
+
