f0beb31a36309d87ccd085ba9ba1517c9f666456
cburny <claire.burny@ens-lyon.fr>
2017-09-21 09:44:44 +0200

    remove_duplicates.sh: call rmdup locally with flagstat report

--- src/remove_duplicates.sh
+++ src/remove_duplicates.sh
@@ -1,37 +1,12 @@
-#$ -S /bin/bash
-### nom du job:
-#$ -N remove_duplicates
-### file d'attente:
-#$ -q E5-2670deb128*
-### parallel environnement & nslots
-#$ -pe openmp16 16
-### charger l'environnement utilisateur pour SGE
-#$ -cwd
-### exporte les variables d'environnement sur les noeuds d'exécution
-#$ -V
-### change logs folder
-#$ -o /home/cburny/logs
-#$ -e /home/cburny/logs
+# Remove duplicate
+# Done locally due to problems with PSMN, using samtools 1.3
 
-# initialiser environnement Module
-source /usr/share/modules/init/bash
-module use /applis/PSMN/Modules
-module load Base/psmn
-module load SAMtools/1.3.1
-module list
-umask 002
-
-INPUT_BAM="/scratch/cburny/Output_Mbelari/2017_09_12_MRDR6_trim_Mbelari_mapped.bam"
-OUTPUT="/scratch/cburny/Output_Mbelari/2017_09_13_MRDR6_trim_Mbelari_mapped_sort"
-OUTPUT_BAM="/scratch/cburny/Output_Mbelari/2017_09_13_MRDR6_trim_Mbelari_mapped_sort.bam"
-OUTPUTrmdup="/scratch/cburny/Output_Mbelari/2017_09_13_MRDR6_trim_Mbelari_mapped_sort_rmdup.bam"
-REPORTrmdup="/scratch/cburny/Output_Mbelari/2017_09_13_MRDR6_trim_Mbelari_mapped_sort_rmdup_flagstat.txt"
-
-##### Sort and index
-samtools sort $INPUT_BAM $OUTPUT &&\
-samtools index $OUTPUT_BAM &&\
+##### Args #####
+# $1: input BAM
+# $2: output BAM without PCR duplicates
+# $3: flagstat file output on $2
 
 ##### remove of duplicates
-samtools rmdup $OUTPUT_BAM $OUTPUTrmdup &&\
+samtools rmdup $1 $2 &&\
 
-samtools flagstat $OUTPUTrmdup > $REPORTrmdup
+samtools flagstat $2 > $3
