702aa1e1fa199157c5c08a62625d6e7f0cbe073d
cburny <claire.burny@ens-lyon.fr>
2017-09-21 09:59:45 +0200

    change_BAM_header.sh: update code to run locally but not used

--- src/change_BAM_header.sh
+++ src/change_BAM_header.sh
@@ -1,15 +1,12 @@
 # Pich header from BAM
-samtools view -H results/mapping/without_duplicates/2017_09_13_MRDR5_trim_Mbelari_mapped_sort_rmdup.bam > tmp/2017_09_18_MRDR5_trim_Mbelari_mapped_sort_rmdup_header.sam &&\
-samtools view -H results/mapping/without_duplicates/2017_09_13_MRDR6_trim_Mbelari_mapped_sort_rmdup.bam > tmp/2017_09_18_MRDR6_trim_Mbelari_mapped_sort_rmdup_header.sam &&\
+samtools view -H results/mapping/without_duplicates/MRDR5_trim_Mbelari_mapped_rmdup_rg.bam > tmp/MRDR5_trim_Mbelari_mapped_rmdup_rg_header.sam &&\
+samtools view -H results/mapping/without_duplicates/MRDR6_trim_Mbelari_mapped_rmdup_rg.bam > tmp/MRDR6_trim_Mbelari_mapped_rmdup_rg_header.sam &&\
 
 # Remove lines within header which are not associated to M.belari contigs
-awk -F "\t" '$1 == "@HD" || $2 ~ /^SN:M/ {print $0}' tmp/2017_09_18_MRDR5_trim_Mbelari_mapped_sort_rmdup_header.sam > tmp/2017_09_18_MRDR5_reheader.sam &&\
-awk -F "\t" '$1 == "@HD" || $2 ~ /^SN:M/ {print $0}' tmp/2017_09_18_MRDR6_trim_Mbelari_mapped_sort_rmdup_header.sam > tmp/2017_09_18_MRDR6_reheader.sam &&\
+awk -F "\t" '$1 == "@HD" || $1 == "@RG" || $2 ~ /^SN:M/ {print $0}' tmp/MRDR6_trim_Mbelari_mapped_rmdup_rg_header.sam > results/mapping/without_duplicates/MRDR6_reheader.sam &&\
+awk -F "\t" '$1 == "@HD" || $1 == "@RG" || $2 ~ /^SN:M/ {print $0}' tmp/MRDR5_trim_Mbelari_mapped_rmdup_rg_header.sam > results/mapping/without_duplicates/MRDR5_reheader.sam &&\
 
 # Change header
-samtools reheader tmp/2017_09_18_MRDR5_reheader.sam results/mapping/without_duplicates/2017_09_13_MRDR5_trim_Mbelari_mapped_sort_rmdup.bam > results/mapping/without_duplicates/2017_09_18_MRDR5_trim_Mbelari_mapped_sort_rmdup.bam
-
-samtools reheader tmp/2017_09_18_MRDR6_reheader.sam results/mapping/without_duplicates/2017_09_13_MRDR6_trim_Mbelari_mapped_sort_rmdup.bam > results/mapping/without_duplicates/2017_09_18_MRDR6_trim_Mbelari_mapped_sort_rmdup.bam
-
-
+samtools reheader results/mapping/without_duplicates/MRDR5_reheader.sam results/mapping/without_duplicates/MRDR5_trim_Mbelari_mapped_rmdup_rg.bam > results/mapping/without_duplicates/MRDR5_trim_Mbelari_mapped_rmdup_rgh.bam &&\
+samtools reheader results/mapping/without_duplicates/MRDR6_reheader.sam results/mapping/without_duplicates/MRDR6_trim_Mbelari_mapped_rmdup_rg.bam > results/mapping/without_duplicates/MRDR6_trim_Mbelari_mapped_rmdup_rgh.bam
 
