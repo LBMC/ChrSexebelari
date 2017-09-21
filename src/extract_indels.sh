75df110d30f66856af963449c56a7ff25146aa9c
cburny <claire.burny@ens-lyon.fr>
2017-09-21 10:10:25 +0200

    extract_indels.sh: extract indels from vcf.gz files, no more used

--- src/extract_indels.sh
+++ src/extract_indels.sh
@@ -1,3 +1,5 @@
-./bin/vcftools --vcf results/call_var/2017_09_13_MRDR5_trim_Mbelari_mapped_sort_rmdup.vcf --keep-only-indels --recode-INFO-all --out results/call_var/MRDR5_trim_Mbelari_mapped_sort_rmdup_indels --recode &&\
+./bin/vcftools --gzvcf results/call_var/MRDR5_trim_Mbelari_mapped_rmdup.vcf.gz --keep-only-indels --recode --recode-INFO-all | gzip -c > results/call_var/MRDR5_trim_Mbelari_mapped_rmdup_indels.vcf.gz &&\
 
-./bin/vcftools --vcf results/call_var/2017_09_13_MRDR6_trim_Mbelari_mapped_sort_rmdup.vcf --keep-only-indels --recode-INFO-all --out results/call_var/MRDR6_trim_Mbelari_mapped_sort_rmdup_indels --recode 
+./bin/vcftools --gzvcf results/call_var/MRDR6_trim_Mbelari_mapped_rmdup.vcf.gz --keep-only-indels --recode --recode-INFO-all | gzip -c > results/call_var/MRDR6_trim_Mbelari_mapped_rmdup_indels.vcf.gz 
+
+#./bin/vcftools --gvcf results/call_var/2017_09_13_MRDR6_trim_Mbelari_mapped_sort_rmdup.vcf --keep-only-indels --recode-INFO-all --out results/call_var/MRDR6_trim_Mbelari_mapped_sort_rmdup_indels --recode 
