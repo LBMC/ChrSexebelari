60b7f4e86cb7da43fa717ed2d9b7e765c9c24153
cburny <claire.burny@ens-lyon.fr>
2017-09-21 10:09:31 +0200

    summary-vcf_file.sh: summary vcf before and after realignement around indels

--- src/summary-vcf_file.sh
+++ src/summary-vcf_file.sh
@@ -1,5 +1,13 @@
-bin/rtg vcfstats results/call_var/2017_09_13_MRDR5_trim_Mbelari_mapped_sort_rmdup.vcf > results/call_var/summary_raw_var_female_JU2917.txt &&\
+##### raw variants 
 
-bin/rtg vcfstats results/call_var/2017_09_13_MRDR6_trim_Mbelari_mapped_sort_rmdup.vcf > results/call_var/summary_raw_var_male_JU2917.txt
+bin/rtg vcfstats results/call_var/MRDR5_trim_Mbelari_mapped_rmdup.vcf.gz > results/call_var/summary_raw_var_female_JU2917.txt &&\
+
+bin/rtg vcfstats results/call_var/MRDR6_trim_Mbelari_mapped_rmdup.vcf.gz > results/call_var/summary_raw_var_male_JU2917.txt &&\
+
+##### after realignment around indels
+
+bin/rtg vcfstats results/call_var/MRDR5_trim_Mbelari_mapped_rmdup_rg_realign_indels.vcf.gz > results/call_var/summary_realign_indels_var_female_JU2917.txt &&\
+
+bin/rtg vcfstats results/call_var/MRDR6_trim_Mbelari_mapped_rmdup_rg_realign_indels.vcf.gz > results/call_var/summary_realign_indels_var_male_JU2917.txt
 
 
