684dcfdc4f566f9fab9c375a0f1598d00739147a
cburny <claire.burny@ens-lyon.fr>
2017-09-21 09:51:48 +0200

    picard_add_read_groups.sh: add RG from input parameters from users

--- src/picard_add_read_groups.sh
+++ src/picard_add_read_groups.sh
@@ -1,34 +1,19 @@
-#!/bin/bash
-### shell du job:
-#$ -S /bin/bash
-### nom du job:
-#$ -N PICARD_RGtag
-### file d'attente:
-#$ -q E5-2670deb128*
-### parallel environnement & nslots
-#$ -pe mpi16_debian 16 
-### charger l'environnement utilisateur pour SGE
-#$ -cwd
-### exporte les variables d'environnement sur les noeuds d'exécution
-#$ -V
-### mails en debut et fin d execution
-#$ -m be
-### change logs folder
-#$ -o /home/cburny/logs
-#$ -e /home/cburny/logs
+# Add reads group
+# Done locally due to problems with PSMN, using picard 2.12.1. 
+# Obtained on 20 sept 2017: 
+# git clone https://github.com/broadinstitute/picard.git
+#    cd picard/
+#    ./gradlew shadowJar
+# copy build/libs/picard.jar to bin
 
-# initialiser environnement Module
-source /usr/share/modules/init/bash
-module use /applis/PSMN/Modules
-module load Base/psmn 
-module load picard/2.3.0 
-module list
-umask 002
+##### Args #####
+# $1: input BAM
+# $2: id, ex: HKCVHBBXX.1
+# $3: lb, ex: male
+# $4: pu, ex: HKCVHBBXX.1
+# $5: sm, ex: MRDR6
+# $6: output BAM without RG in header
 
-PICARDHOME="/applis/PSMN/generic/picard/2.3.0"
-INPUT="/scratch/cburny/Output_Mbelari/2017_09_18_MRDR6_trim_Mbelari_mapped_sort_rmdup.bam"
-OUTPUT="/scratch/cburny/Output_Mbelari/2017_09_18_MRDR6_trim_Mbelari_mapped_rmdup_rg.bam"
+java -jar bin/picard.jar AddOrReplaceReadGroups I=$1 O=$6 RGID=$2 RGLB=$3 RGPL=illumina RGPU=$4 RGSM=$5 VALIDATION_STRINGENCY=SILENT CREATE_INDEX=true
 
-java -jar $PICARDHOME/picard.jar AddOrReplaceReadGroups I=$INPUT O=$OUTPUT RGID=HKCVHBBXX.1 RGLB=male RGPL=illumina RGPU=HKCVHBBXX.1 RGSM=MRDR6 VALIDATION_STRINGENCY=LENIENT
 
-echo "termine"
