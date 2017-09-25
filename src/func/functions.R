c4ade7f61293bea2e4d8a596e1d39124000f69a8
cburny <claire.burny@ens-lyon.fr>
2017-09-25 12:03:19 +0200

    functions.R: read vcf to extract allele counts

--- src/func/functions.R
+++ src/func/functions.R
@@ -1,4 +1,16 @@
-##### CoverageStat #####
-CoverageStat <- function(){
+##### ExtractCountFromVCF #####
+ExtractCountFromVCF <- function(vcf.without.header, sexe){
   
+  info <- unname(do.call(rbind, sapply(vcf.without.header$INFO, function(x) {
+    eval(parse(text = paste("DP4=c(", strsplit(strsplit(x, "DP4=")[[1]][2], ";")[[1]][1], ")", sep = "")));
+    is.INDEL <- length(grep("INDEL", x));
+    ref = sum(DP4[1:2]);
+    alt = sum(DP4[3:4]);
+    tot=sum(DP4);
+    AN = strsplit(strsplit(x, "AN=")[[1]][2], ";")[[1]][1];
+    as.numeric(c(AN, ref, alt, tot, is.INDEL))
+  }, simplify = F)))
+  info <- as.data.frame(info)
+  colnames(info) <- c("nb.alleles", paste("count.ref.", sexe, sep = ""), paste("count.alt.", sexe, sep = ""), paste("tot.", sexe, sep = ""), "is.INDEL")
+  return(info)
 }
