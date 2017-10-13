d968931688a9e480359163eb62b7e6caafc9b3a3
Laurent Modolo <laurent.modolo@ens-lyon.fr>
2017-10-13 10:53:26 +0200

    dnaPipeTE.sh: save pipeline stdout in report file

--- src/pipe/dnaPipeTE.sh
+++ src/pipe/dnaPipeTE.sh
@@ -5,17 +5,17 @@ mkdir -p results/dnaPipeTE/female_JU2817
 python3 src/dnaPipeTE/dnaPipeTE.py \
 -input results/mapping/mapped/2017_09_13_MRDR5_trim_Mbelari_mapped_sort.fastq.gz \
 -output results/dnaPipeTE/female_JU2817 \
--cpu 12 \
+-cpu 10 \
 -genome_size 162071050 \
 -genome_coverage 22.9 \
--sample_number 3
+-sample_number 3 > results/female_JU2817_report.txt
 
 # male_JU2817
 mkdir -p results/dnaPipeTE/male_JU2817
 python3 src/dnaPipeTE/dnaPipeTE.py \
 -input results/mapping/mapped/2017_09_13_male_JU2817_R1.fastq.gz \
 -output results/dnaPipeTE/male_JU2817 \
--cpu 12 \
+-cpu 10 \
 -genome_size 162071050 \
 -genome_coverage 13.6 \
--sample_number 5
+-sample_number 5 > results/male_JU2817_report.txt
