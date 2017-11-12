c52b291852bf062ac66570b5d690973137b948b9
cburny <claire.burny@ens-lyon.fr>
2017-11-12 16:57:09 +0100

    call_var_Mbelari.sh: correct comment

--- src/call_var_Mbelari.sh
+++ src/call_var_Mbelari.sh
@@ -6,7 +6,7 @@ samtools mpileup -t DP,AD,ADF,ADR,SP,INFO/AD,INFO/ADF,INFO/ADR -ABQ0 -L 100000 -
 
 samtools mpileup -t DP,AD,ADF,ADR,SP,INFO/AD,INFO/ADF,INFO/ADR -ABQ0 -L 100000 -uf data/ReferenceGenomes/Mesorhabditis_belari_JU2817_v2_scaffolds_repeatmasker_masked.fa results/mapping/without_duplicates/MRDR6_trim_Mbelari_mapped_rmdup.bam | bcftools call -mv --output-type z -p 0.1 -o results/call_var/MRDR6_trim_Mbelari_mapped_rmdup.vcf.gz &&\
 
-# To call variants (-mv) and get pileup o, vCF format (-mA) after realignment around INDELs: samtools mpileup -t DP,AD,ADF,ADR,SP,INFO/AD,INFO/ADF,INFO/ADR -ABQ0 -L 100000 -uf $1 $2 | bcftools call -mA --output-type z -p 0.1 -o $3
+# To call variants (-mv) and get pileup in VCF format (-mA) after realignment around INDELs: samtools mpileup -t DP,AD,ADF,ADR,SP,INFO/AD,INFO/ADF,INFO/ADR -ABQ0 -L 100000 -uf $1 $2 | bcftools call -mA --output-type z -p 0.1 -o $3
 samtools mpileup -t DP,AD,ADF,ADR,SP,INFO/AD,INFO/ADF,INFO/ADR -ABQ0 -L 100000 -uf data/ReferenceGenomes/Mesorhabditis_belari_JU2817_v2_scaffolds_repeatmasker_masked.fa results/mapping/without_duplicates/MRDR5_trim_Mbelari_mapped_rmdup_rg_realign_indels.bam | bcftools call -mv --output-type z -p 0.1 -o results/call_var/MRDR5_trim_Mbelari_mapped_rmdup_rg_realign_indels.vcf.gz &&\
 
 samtools mpileup -t DP,AD,ADF,ADR,SP,INFO/AD,INFO/ADF,INFO/ADR -ABQ0 -L 100000 -uf data/ReferenceGenomes/Mesorhabditis_belari_JU2817_v2_scaffolds_repeatmasker_masked.fa results/mapping/without_duplicates/MRDR6_trim_Mbelari_mapped_rmdup_rg_realign_indels.bam | bcftools call -mv --output-type z -p 0.1 -o results/call_var/MRDR6_trim_Mbelari_mapped_rmdup_rg_realign_indels.vcf.gz &&\
@@ -14,5 +14,5 @@ samtools mpileup -t DP,AD,ADF,ADR,SP,INFO/AD,INFO/ADF,INFO/ADR -ABQ0 -L 100000 -
 
 samtools mpileup -t DP,AD,ADF,ADR,SP,INFO/AD,INFO/ADF,INFO/ADR -ABQ0 -L 100000 -uf data/ReferenceGenomes/Mesorhabditis_belari_JU2817_v2_scaffolds_repeatmasker_masked.fa results/mapping/without_duplicates/MRDR5_trim_Mbelari_mapped_rmdup_rg_realign_indels.bam | bcftools call -mA --output-type z -p 0.1 -o results/call_var/MRDR5_trim_Mbelari_mapped_rmdup_rg_realign_indels_A.vcf.gz &&\
 
-samtools mpileup -t DP,AD,ADF,ADR,SP,INFO/AD,INFO/ADF,INFO/ADR -ABQ0 -L 100000 -uf data/ReferenceGenomes/Mesorhabditis_belari_JU2817_v2_scaffolds_repeatmasker_masked.fa results/mapping/without_duplicates/MRDR6_trim_Mbelari_mapped_rmdup_rg_realign_indels.bam | bcftools call -mA --output-type z -p 0.1 -o results/call_var/MRDR6_trim_Mbelari_mapped_rmdup_rg_realign_indels_A.vcf.gz &&\
+samtools mpileup -t DP,AD,ADF,ADR,SP,INFO/AD,INFO/ADF,INFO/ADR -ABQ0 -L 100000 -uf data/ReferenceGenomes/Mesorhabditis_belari_JU2817_v2_scaffolds_repeatmasker_masked.fa results/mapping/without_duplicates/MRDR6_trim_Mbelari_mapped_rmdup_rg_realign_indels.bam | bcftools call -mA --output-type z -p 0.1 -o results/call_var/MRDR6_trim_Mbelari_mapped_rmdup_rg_realign_indels_A.vcf.gz 
 
