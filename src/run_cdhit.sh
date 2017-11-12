25f958b2c64714c54a7373027b92584a7038dc5e
cburny <claire.burny@ens-lyon.fr>
2017-11-12 17:21:02 +0100

    run_cdhit.sh: script to run cdhit needed to be call on PSMN after having installed cdhit module

---
+++ src/run_cdhit.sh
@@ -0,0 +1,4 @@
+# cdhit v4.6.8 
+
+bin/cdhit/cd-hit-est-2d -i results/mapping/mapped/2017_11_06_MRDR5_trim_Mbelari_Ecoli_unmapped.fa -i2 results/mapping/mapped/2017_11_06_MRDR6_trim_Mbelari_Ecoli_unmapped.fa -o results/mapping/mapped/2017_11_06_cdhit.fa -P 0 -T 4 -M 16000 -c 0.9 -n 9 -l 10 
+
