87e5de7adad0fecd509c74c81a2acf6ad5746d23
cburny <claire.burny@ens-lyon.fr>
2017-11-13 13:21:00 +0100

    prep_fisher.R: output count files for variants present in male and/or female pool and add column if ALT allele male = ALT allele female

--- src/prep_fisher.R
+++ src/prep_fisher.R
@@ -5,6 +5,7 @@ library(doParallel)
 library(gridExtra)
 
 size.genome.mbelari <- read.table("data/ReferenceGenomes/2017_09_13_Mbelari.sizes.genome", h = F, sep = "\t", stringsAsFactors = F)
+size.contigs1000 <- size.genome.mbelari$V1[which(size.genome.mbelari$V2 >= 1000)]
 
 do.prep.fisher <- F
 
@@ -15,38 +16,32 @@ if (do.prep.fisher) {
  
   ##### Look at common / biallelic genic sites between pools
   merge.sexe.common <- merge(tab.variants.male[, c("CHROM", "POS", "REF", "ALT", "QUAL", "nb.alleles", "count.ref.male", "count.alt.male", "tot.male", "genes")], tab.variants.female[, c("CHROM", "POS", "REF", "ALT", "QUAL", "nb.alleles", "count.ref.female", "count.alt.female", "tot.female", "is.INDEL", "genes")], by = c("CHROM",  "POS", "REF", "genes"))
-  ind.genic <- which(merge.sexe.common$genes != "")
-  nb.SNPs.all.common = table(merge.sexe.common$is.INDEL)["0"]
-  nb.INDELs.all.common =  table(merge.sexe.common$is.INDEL)["1"]
-  nb.SNPs.genic.common = table(merge.sexe.common[ind.genic]$is.INDEL)["0"]
-  nb.INDELs.genic.common = table(merge.sexe.common[ind.genic]$is.INDEL)["1"]
-  nb.ctg.SNPs.all.common <- length(unique(merge.sexe.common$CHROM[which(merge.sexe.common$is.INDEL == 0)]))
-  nb.ctg.INDELs.all.common <- length(unique(merge.sexe.common$CHROM[which(merge.sexe.common$is.INDEL == 1)]))
+  summ1 <- SummarizeSNPsINDELsWithinDataFrame(merge.sexe.common, suff = "shared position") 
+
+  ##### Add counts for variants present in one pool and not in the other 
+  merge.sexe.all <- merge(tab.variants.male[, c("CHROM", "POS", "REF", "ALT", "QUAL", "nb.alleles", "count.ref.male", "count.alt.male", "tot.male", "genes")], tab.variants.female[, c("CHROM", "POS", "REF", "ALT", "QUAL", "nb.alleles", "count.ref.female", "count.alt.female", "tot.female", "is.INDEL", "genes")], by = c("CHROM",  "POS", "REF", "genes"), all.x = T, all.y = T)
+  summ1b <- SummarizeSNPsINDELsWithinDataFrame(merge.sexe.all, suff = "all position") 
 
   merge.sexe.common.biallelic <- merge(tab.variants.male[, c("CHROM", "POS", "REF", "ALT", "QUAL", "nb.alleles", "count.ref.male", "count.alt.male", "tot.male", "genes")], tab.variants.female[, c("CHROM", "POS", "REF", "ALT", "QUAL", "nb.alleles", "count.ref.female", "count.alt.female", "tot.female", "is.INDEL", "genes")], by = c("CHROM",  "POS", "REF", "ALT", "genes"))
-  ind.genic <- which(merge.sexe.common.biallelic$genes != "")
-  nb.SNPs.all.common.biallelic = table(merge.sexe.common.biallelic$is.INDEL)["0"]
-  nb.INDELs.all.common.biallelic =  table(merge.sexe.common.biallelic$is.INDEL)["1"]
-  nb.SNPs.genic.common.biallelic = table(merge.sexe.common.biallelic[ind.genic]$is.INDEL)["0"]
-  nb.INDELs.genic.common.biallelic = table(merge.sexe.common.biallelic[ind.genic]$is.INDEL)["1"]
-  nb.ctg.SNPs.all.common.biallelic <- length(unique(merge.sexe.common.biallelic$CHROM[which(merge.sexe.common.biallelic$is.INDEL == 0)]))
-  nb.ctg.INDELs.all.common.biallelic <- length(unique(merge.sexe.common.biallelic$CHROM[which(merge.sexe.common.biallelic$is.INDEL == 1)]))
+  summ2 <- SummarizeSNPsINDELsWithinDataFrame(merge.sexe.common.biallelic, suff = "shared position and alt allele") 
+ 
+  ##### Apply filter on contig size, DP (>2 in total count) and distance to INDEL
+  tmp.common.biallelic <- merge.sexe.common.biallelic[which((merge.sexe.common.biallelic$tot.male+merge.sexe.common.biallelic$tot.female)>2 & merge.sexe.common.biallelic$CHROM %in% size.contigs1000), ]
+  tmp.common.biallelic$ID <- 1:dim(tmp.common.biallelic)[1]
+  summ3 <- SummarizeSNPsINDELsWithinDataFrame(tmp.common.biallelic, suff = "shared position and alt allele, ctg>=1000, DP>2") 
 
-  ##### Apply filter on contig size, DP (>1 in each pool) and distance to INDEL
-  tmp <- merge.sexe.common.biallelic[which(merge.sexe.common.biallelic$tot.male>1 & merge.sexe.common.biallelic$tot.female>1), ]
-  tmp$ID <- 1:dim(tmp)[1]
-	
-  size.contigs1000 <- size.genome.mbelari$V1[which(size.genome.mbelari$V2 >= 1000)]
-  tmp <- tmp[which(tmp$CHROM %in% size.contigs1000), ]
+  tmp.all <- merge.sexe.all[which((merge.sexe.all$tot.male+merge.sexe.all$tot.female)>2 & merge.sexe.all$CHROM %in% size.contigs1000), ]
+  tmp.all$ID <- 1:dim(tmp.all)[1]
+  summ2b <- SummarizeSNPsINDELsWithinDataFrame(tmp.all, suff = "all position, ctg>=1000, DP>2") 
 
   ##### Compute distance to the nearest INDELs to remove SNP at 1bp away from INDEL and remove INDELs
   cores=detectCores()
   cl <- makeCluster(cores[1]-1) #not to overload your computer
   registerDoParallel(cl)
-  remove.dist <- foreach(i=1:dim(tmp)[1], .combine=c) %dopar% {
-  	tmp2 <- tmp[i, ]
+  remove.dist <- foreach(i=1:dim(tmp.all)[1], .combine=c) %dopar% {
+  	tmp2 <- tmp.all[i, ]
   	if(tmp2$is.INDEL == 0) {
-		temp <- tmp[which(tmp$CHROM == tmp2$CHROM & tmp$is.INDEL == 1), ]
+		temp <- tmp.all[which(tmp.all$CHROM == tmp2$CHROM & tmp.all$is.INDEL == 1), ]
 		d <- temp$POS-tmp2$POS
 		dist <- min(abs(d))
 		if (dist <= 1){
@@ -62,46 +57,40 @@ if (do.prep.fisher) {
   stopCluster(cl)
 
   ids.remove <- remove.dist[-which(is.na(remove.dist))]
-  merge.sexe.common.biallelic.filt <- tmp[-which(tmp$ID %in% ids.remove), ]
-  ind.genic <- which(merge.sexe.common.biallelic.filt$genes != "")
-  nb.SNPs.all.common.biallelic.filt = table(merge.sexe.common.biallelic.filt$is.INDEL)["0"]
-  nb.INDELs.all.common.biallelic.filt =  table(merge.sexe.common.biallelic.filt$is.INDEL)["1"]
-  nb.SNPs.genic.common.biallelic.filt = table(merge.sexe.common.biallelic.filt[ind.genic]$is.INDEL)["0"]
-  nb.INDELs.genic.common.biallelic.filt = table(merge.sexe.common.biallelic.filt[ind.genic]$is.INDEL)["1"]
-  nb.ctg.SNPs.all.common.biallelic.filt <- length(unique(merge.sexe.common.biallelic.filt$CHROM[which(merge.sexe.common.biallelic.filt$is.INDEL == 0)]))
-  nb.ctg.INDELs.all.common.biallelic.filt <- length(unique(merge.sexe.common.biallelic.filt$CHROM[which(merge.sexe.common.biallelic.filt$is.INDEL == 1)]))
+  merge.sexe.all.filt <- tmp.all[-which(tmp.all$ID %in% ids.remove), ]
+  summ3b <- SummarizeSNPsINDELsWithinDataFrame(merge.sexe.all.filt, suff = "all position, ctg>=1000, DP>2, dist.to.INDEL>1") 
 
-  summary.filt <- data.frame(nb.SNPs.all.common = nb.SNPs.all.common, nb.INDELs.all.common =  nb.INDELs.all.common, nb.SNPs.genic.common = nb.SNPs.genic.common,
-  nb.INDELs.genic.common = nb.INDELs.genic.common, nb.ctg.SNPs.all.common = nb.ctg.SNPs.all.common, nb.ctg.INDELs.all.common = nb.ctg.INDELs.all.common,
-  nb.SNPs.all.common.biallelic = nb.SNPs.all.common.biallelic, nb.INDELs.all.common.biallelic =  nb.INDELs.all.common.biallelic, nb.SNPs.genic.common.biallelic = nb.SNPs.genic.common.biallelic, nb.INDELs.genic.common.biallelic = nb.INDELs.genic.common.biallelic, nb.ctg.SNPs.all.common.biallelic = nb.ctg.SNPs.all.common.biallelic,
-  nb.ctg.INDELs.all.common.biallelic = nb.ctg.INDELs.all.common.biallelic, nb.SNPs.all.common.biallelic.filt = nb.SNPs.all.common.biallelic.filt,
-  nb.INDELs.all.common.biallelic.filt =  nb.INDELs.all.common.biallelic.filt, nb.SNPs.genic.common.biallelic.filt = nb.SNPs.genic.common.biallelic.filt,
-  nb.INDELs.genic.common.biallelic.filt = nb.INDELs.genic.common.biallelic.filt, nb.ctg.SNPs.all.common.biallelic.filt = nb.ctg.SNPs.all.common.biallelic.filt,
-  nb.ctg.INDELs.all.common.biallelic.filt = nb.ctg.INDELs.all.common.biallelic.filt, stringsAsFactors = F)
+  summary.filt <- rbind(summ1, summ2, summ3, summ1b, summ2b, summ3b)
 
-  write.table(summary.filt, "results/call_var/2017_11_10_summary_filt_variants.txt", sep = "\t",  quote = F, row.names = F)
-  write.table(merge.sexe.common.biallelic.filt, "results/call_var/2017_11_09_merge.sexe.common.biallelic.filt.txt", sep = "\t", row.names = F, col.names = T, quote = F)
-  write.table(merge.sexe.common.biallelic.filt[which(merge.sexe.common.biallelic.filt$is.INDEL == 0),], "results/call_var/2017_11_09_merge.sexe.common.biallelic.filt.SNP.txt", sep = "\t", row.names = F, col.names = T, quote = F)
+  ##### Add same.ALT column if ALT allele differs between male and female
+  merge.sexe.all.filt$same.ALT <- 1
+  merge.sexe.all.filt$same.ALT[which((merge.sexe.all.filt$ALT.x == merge.sexe.all.filt$ALT.y) == F)] <- 0
+
+  tab.snp <- table(merge.sexe.all.filt$same.ALT[which(merge.sexe.all.filt$is.INDEL == 0)])
+  tab.indel <- table(merge.sexe.all.filt$same.ALT[which(merge.sexe.all.filt$is.INDEL == 1)])
+  pdf("results/call_var/2017_11_13_summary_alleles_ALT_between_pools.pdf", height=4, width=10)
+  par(mfrow = c(1, 2))
+  bb.snp <- barplot(tab.snp, names.arg = c("different ALT allele", "same ALT allele"), main = "SNP identity between both pools", cex.arg = 0.75)
+  text(bb.snp, max(tab.snp)/2,labels=tab.snp) 
+  bb.indel <- barplot(tab.indel, names.arg = c("different ALT allele", "same ALT allele"), main = "INDELs identity between both pools", cex.arg = 0.75) 
+  text(bb.indel, max(tab.indel)/2,labels=tab.indel) 
+  dev.off()
+
+  write.table(summary.filt, "results/call_var/2017_11_13_summary_filt_variants.txt", sep = "\t",  quote = F, row.names = F)
+  write.table(merge.sexe.all.filt, "results/call_var/2017_11_13_merge.sexe.all.filt.txt", sep = "\t", row.names = F, col.names = T, quote = F)
+  write.table(merge.sexe.all.filt[which(merge.sexe.all.filt$is.INDEL == 0),], "results/call_var/2017_11_13_merge.sexe.all.filt.SNP.txt", sep = "\t", row.names = F, col.names = T, quote = F)
 }
 
-f <- read.table("results/call_var/2017_11_10_summary_filt_variants.txt", sep = "\t", h = T)
-pdf("results/call_var/2017_11_10_summary_filt1_variants.pdf", height=4, width=14)
-grid.table(f[, 1:6])
-dev.off()
-
-pdf("results/call_var/2017_11_10_summary_filt2_variants.pdf", height=4, width=14)
-grid.table(f[, 7:12])
-dev.off()
-
-pdf("results/call_var/2017_11_10_summary_filt3_variants.pdf", height=4, width=14)
-grid.table(f[, 13:18])
+f <- read.table("results/call_var/2017_11_13_summary_filt_variants.txt", sep = "\t", h = T)
+pdf("results/call_var/2017_11_13_summary_filt_variants.pdf", height=4, width=12)
+grid.table(f)
 dev.off()
 
 # Plot histogram of frequency of filtered variants 
-data.filt.SNPs <- read.csv("results/call_var/2017_11_09_merge.sexe.common.biallelic.filt.SNP.txt", sep = "\t", h = T, stringsAsFactors = F)
+data.filt.SNPs <- read.csv("results/call_var/2017_11_09_merge.sexe.all.filt.SNP.txt", sep = "\t", h = T, stringsAsFactors = F)
 
 pdf("results/call_var/2017_11_13_merge.sexe.common.biallelic.filt.SNP.frequency.male.female.pool.pdf")
-hist(data.filt.SNPs$count.alt.male/data.filt.SNPs$tot.male, col = adjustcolor("blue", 0.75), breaks = 100, xlim = c(0,1), xlab = "Variants frequency", main = "Histogram of filtered SNPs frequencies")
-hist(data.filt.SNPs$count.alt.female/data.filt.SNPs$tot.female, col = adjustcolor("red", 0.75), add = T, breaks = 100, xlim = c(0,1))
+hist(data.filt.SNPs$count.alt.male/data.filt.SNPs$tot.male, col = adjustcolor("blue", 0.75), breaks = 20, xlim = c(0,1), xlab = "Variants frequency", main = "Histogram of filtered SNPs frequencies")
+hist(data.filt.SNPs$count.alt.female/data.filt.SNPs$tot.female, col = adjustcolor("red", 0.75), add = T, breaks = 20, xlim = c(0,1))
 legend("topleft", c("male variants frequency", "female variants frequency"), fill = c("blue", "red"))
 dev.off()
