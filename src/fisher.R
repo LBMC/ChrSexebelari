c0a38d0eed8d7764ed230a96368d945d817a495c
cburny <claire.burny@ens-lyon.fr>
2017-11-19 18:39:51 +0100

    fisher.R: compute 10 datasets under H0 with best alpha value

--- src/fisher.R
+++ src/fisher.R
@@ -91,7 +91,7 @@ do.H0 <- F
 if(do.H0){
 	data.tests <- read.csv("results/call_var/2017_11_14_merge.sexe.common.biallelic.filt.SNP.same.alt.fisher.G.txt", sep = "\t", h = T, stringsAsFactors = F)
 	p.male <- mean(data.tests$tot.male)/(mean(data.tests$tot.female)+mean(data.tests$tot.male))
-	alpha <- c(5, 10, 15, 20, 25, 30, 35, 40)
+	alpha <- c(5, 10, 15, 20, 25, 30, 35, 40, 45, 55, 60, 70, 75, 80)
 	for (alphanull in alpha) {
 		cores=detectCores()
 		cl <- makeCluster(cores[1]-1) #not to overload your computer
@@ -120,29 +120,77 @@ if(do.H0){
 	}
 
 	# Choose alpha
-        h <- hist(data.tests$pval.fisher.raw.count, breaks = seq(0, 1, length = 1000), plot = F, include.lowest = T, right = T)
+        h <- hist(data.tests$pval.fisher.raw.count, breaks = seq(0, 1, length = 700), plot = F, include.lowest = T, right = T)
 	p.bins <- h$counts/dim(data.tests)[1]
 	res.choose.alpha <- NULL
 	for (alphanull in alpha) {
 		tmp <- readRDS(paste("results/fisher/2017_11_14_simulated_count_H0_from_Bastide_alpha", alphanull, "_biallelic.filt.SNP.same.alt.fisher.RData", sep = ""))
 
-        	htmp <- hist(tmp$pval.fisher.H0, seq(0, 1, length = 1000), plot = F, include.lowest = T, right = T)
+        	htmp <- hist(tmp$pval.fisher.H0, seq(0, 1, length = 700), plot = F, include.lowest = T, right = T)
 		ptest.chisq <- chisq.test(htmp$counts, p = p.bins, correct = F)$p.value
-		ptest.fisher <- fisher.test(cbind(htmp$counts, h$counts), simulate.p.value = T, B = 10000)$p.value
+		ptest.fisher <- fisher.test(cbind(htmp$counts, h$counts), simulate.p.value = T)$p.value
+		ptest.ks <- ks.test(htmp$counts, h$counts)$p.value
 
 		# Quantile-quantile plot of observed vs. simulated P-values for the Fisher’s exact test on allele frequency differentiation
 		png(paste("results/fisher/2017_11_14_qqplot_simulated_vs_obs_pvalues_alpha", alphanull, ".png", sep = ""), w = 850)
 		par(mfrow = c(1,2))
-		qqplot(tmp$pval.fisher.H0, data.tests$pval.fisher.raw.count, main = paste("Quantile-quantile plot of observed vs. simulated\nP-values for the Fisher exact test - alpha=", alphanull, sep = ""), xlab = "Simulated null pvalue", ylab = "Observed pvalue")
+		qqplot(tmp$pval.fisher.H0, data.tests$pval.fisher.raw.count, main = paste("Quantile-quantile plot of observed vs. simulated\nP-values for the Fisher exact test - alpha=", alphanull, " on 700 bins", sep = ""), xlab = "Simulated null pvalue", ylab = "Observed pvalue")
 		text(0.6, 0.4, paste("pchisq=", ptest.chisq))
 		text(0.6, 0.2, paste("pfisher=", ptest.fisher))
+		text(0.6, 0.0, paste("pks=", ptest.ks))
 		abline(a=0,b=1,lty=2)
 		qqplot(-log10(tmp$pval.fisher.H0), -log10(data.tests$pval.fisher.raw.count), main = paste("Quantile-quantile plot of observed vs. simulated\n-log10(P-values) for the Fisher exact test - alpha=", alphanull, sep = ""), xlab = "Simulated null -log10(pvalue)", ylab = "Observed -log10(pvalue)")
 		abline(a=0,b=1,lty=2)
 		dev.off()
 
-		res.choose.alpha <- rbind(res.choose.alpha, data.frame(alpha = alphanull, beta = (alphanull-alphanull*p.male)/p.male, pchisq_pvalH0_to_obs = ptest.chisq, pfisher_pvalH0_to_obs = ptest.fisher, stringsAsFactors = F))
+		res.choose.alpha <- rbind(res.choose.alpha, data.frame(alpha = alphanull, beta = (alphanull-alphanull*p.male)/p.male, pchisq_pvalH0_to_obs = ptest.chisq, pfisher_pvalH0_to_obs = ptest.fisher, pks_on_counts_pvalH0_to_obs = ptest.ks, stringsAsFactors = F))
 	}
 	write.table(res.choose.alpha, "results/fisher/2017_11_14_summary_choose_alpha_for_siimulated_count_H0_from_Bastide_biallelic.filt.SNP.same.alt.fisher.txt", sep = "\t", col.names = T, row.names = F, quote = F)
 }
+# Maybe test on 1000bins as well
+
+##### Compute 10 datasets under H0 with alpha = 75
+do.simu.10x <- F
+if(do.simu.10x) {
+	data.tests <- read.csv("results/call_var/2017_11_14_merge.sexe.common.biallelic.filt.SNP.same.alt.fisher.G.txt", sep = "\t", h = T, stringsAsFactors = F)
+	p.male <- mean(data.tests$tot.male)/(mean(data.tests$tot.female)+mean(data.tests$tot.male))
+	alpha.choose <- 75
+	beta.choose <- (alpha.choose-alpha.choose*p.male)/p.male
+	ind.ord <- order(data.tests$pval.fisher.raw.count)
+	pval.obs.ord <- data.tests$pval.fisher.raw.count[ind.ord]
+	data.tests.simu <- data.tests[ind.ord, ]
+	for (i in 1:10) {
+		print(i)
+		cores=detectCores()
+		cl <- makeCluster(cores[1]-1) #not to overload your computer
+		registerDoParallel(cl)
+		pvalH0_out <- foreach(i=1:dim(data.tests)[1], .combine=rbind) %dopar% {
+	          	tmp <- data.tests[i, ]
+	
+			nref <- tmp$count.ref.female + tmp$count.ref.male
+	                beta.choose <- (alpha.choose-alpha.choose*p.male)/p.male
+	                pmref <- rbeta(1, shape1 = alpha.choose, shape2 = beta.choose)
+			nrefmale <- rbinom(n = 1, size = nref, prob = pmref)
+			nreffemale <- nref-nrefmale
+	
+			nalt <- tmp$count.alt.female + tmp$count.alt.male
+                	pmalt <- rbeta(1, shape1 = alpha.choose, shape2 = beta.choose)
+			naltmale <- rbinom(n = 1, size = nalt, prob = pmalt)
+			naltfemale <- nalt-naltmale
+
+ 			data.frame(ID = tmp$ID, count.ref.male = nrefmale, count.alt.male = naltmale, count.ref.female = nreffemale, count.alt.female = naltfemale, stringsAsFactors = F)
+		}
+	  	stopCluster(cl)
+	        pval.fisher.H0 <- ComputePvalueFisher(input.df = pvalH0_out[, c("count.ref.male", "count.alt.male", "count.ref.female", "count.alt.female")])
+	        pvalH0_out$pval.fisher.H0 <- pval.fisher.H0
+
+		saveRDS(pvalH0_out, paste("results/fisher/2017_11_15_simulated_#", i, "_count_H0_alpha", alpha.choose, ".RData", sep = ""))	
+
+		# For the ith rank SNP, # nb simu SNP with pvalue < pvalue obs for Fisher
+		tmp <- data.frame(nb.simu.SNPs.lower.pval = sapply(pval.obs.ord, function(x) length(which(pvalH0_out$pval.fisher.H0<x))), stringsAsFactors = F)
+		saveRDS(tmp, paste("results/fisher/2017_11_15_simulated_compare_to_obs_#", i, "_count_H0_alpha", alpha.choose, ".RData", sep = ""))	
+	}
+}
+
+
 
