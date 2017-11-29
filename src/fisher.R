a77a8d80e52066bb5d3a253e92d4b6b79a24024a
cburny <claire.burny@ens-lyon.fr>
2017-11-29 15:15:02 +0100

    fisher.R: change computation of H0 count tables by choosen parameter from vglm model

--- src/fisher.R
+++ src/fisher.R
@@ -5,6 +5,7 @@ require(qvalue)
 require(data.table)
 require(foreach)
 require(doParallel)
+require(VGAM)
 source("src/func/functions.R")
 
 do.fisher <- F
@@ -20,6 +21,10 @@ if (do.fisher) {
   data.filt.SNP.same.alt$freq.alt.female <- data.filt.SNP.same.alt$count.alt.female/(data.filt.SNP.same.alt$count.ref.female+data.filt.SNP.same.alt$count.alt.female)
   data.filt.SNP.same.alt$freq.alt.male <- data.filt.SNP.same.alt$count.alt.male/(data.filt.SNP.same.alt$count.ref.male+data.filt.SNP.same.alt$count.alt.male)
 
+  ##################
+  ##### Gstats #####
+  ##################
+
   # Compute Gstat
   pseudo.raw <- data.filt.SNP.same.alt[, c("count.ref.male", "count.alt.male", "count.ref.female", "count.alt.female")]
   pseudo.raw <- pseudo.raw+1
@@ -33,14 +38,6 @@ if (do.fisher) {
   for(ctg in uni.ctg) {
     ind <- which(data.filt.SNP.same.alt$CHROM == ctg)
     tmp <- data.filt.SNP.same.alt[ind, c("POS", "gstat.pseudo.raw")]
-    #plot(tmp$POS, tmp$gstat.pseudo.raw)
-    #window <- c(500, 1000, 5000, 10000)
-    #for (j in seq_along(window)) {
-    #  w <- window[j]
-    #  pred <- ksmooth(tmp$POS, tmp$gstat.pseudo.raw, kernel = "normal", bandwidth = w, range.x = range(tmp$POS), n.points = length(tmp$POS), x.points = tmp$POS)
-    #  points(pred$x, pred$y, type = "l", col = j, lty = 2)
-    #}
-    #legend("topright", legend = window, fill = seq_along(window))
     gsmooth <- ksmooth(tmp$POS, tmp$gstat.pseudo.raw, kernel = "normal", bandwidth = w, range.x = range(tmp$POS), n.points = length(tmp$POS), x.points = tmp$POS)
     data.filt.SNP.same.alt$gsmooth.pseudo.raw[ind] <- gsmooth$y
   }
@@ -76,6 +73,10 @@ if (do.fisher) {
   print(h)
   dev.off()
  
+  ##################
+  ##### Fisher #####
+  ##################
+
   # Add raw pvalue associated to Fisher test done on raw counts
   pval.fisher <- ComputePvalueFisher(input.df = data.filt.SNP.same.alt[, c("count.ref.male", "count.alt.male", "count.ref.female", "count.alt.female")])
   data.filt.SNP.same.alt$pval.fisher.raw.count <- pval.fisher
@@ -86,78 +87,22 @@ if (do.fisher) {
   write.table(data.filt.SNP.same.alt, "results/call_var/2017_11_14_merge.sexe.common.biallelic.filt.SNP.same.alt.fisher.G.txt", sep = "\t",  quote = F, row.names = F)
 }
 
-
-##### Compute H0 distrib of count tables from Bastide et al, 2013. alpha != beta, alpha tested from 10 to 40. It concerns FISHER TESTS!!!
-
 do.H0 <- F
-if(do.H0){
-	data.tests <- read.csv("results/call_var/2017_11_14_merge.sexe.common.biallelic.filt.SNP.same.alt.fisher.G.txt", sep = "\t", h = T, stringsAsFactors = F)
-	p.male <- mean(data.tests$tot.male)/(mean(data.tests$tot.female)+mean(data.tests$tot.male))
-	alpha <- c(5, 10, 15, 20, 25, 30, 35, 40, 45, 55, 60, 70, 75, 80)
-	for (alphanull in alpha) {
-		cores=detectCores()
-		cl <- makeCluster(cores[1]-1) #not to overload your computer
-		registerDoParallel(cl)
-		pvalH0_out <- foreach(i=1:dim(data.tests)[1], .combine=rbind) %dopar% {
-	          	tmp <- data.tests[i, ]
-	
-			nref <- tmp$count.ref.female + tmp$count.ref.male
-	                betanull <- (alphanull-alphanull*p.male)/p.male
-	                pmref <- rbeta(1, shape1 = alphanull, shape2 = betanull)
-			nrefmale <- rbinom(n = 1, size = nref, prob = pmref)
-			nreffemale <- nref-nrefmale
-	
-			nalt <- tmp$count.alt.female + tmp$count.alt.male
-                	pmalt <- rbeta(1, shape1 = alphanull, shape2 = betanull)
-			naltmale <- rbinom(n = 1, size = nalt, prob = pmalt)
-			naltfemale <- nalt-naltmale
+##### Compute H0 distribution of count tables: UPDATE: instead using Bastide et al Methods to choose the best alpha value, I use instead a vglm fit on betabinomial to get alpha and beta parameters.
+tests <- read.csv("results/call_var/2017_11_14_merge.sexe.common.biallelic.filt.SNP.same.alt.fisher.FDR.G.txt", sep = "\t", h = T, stringsAsFactors = F)
+fit <- vglm(cbind(c(tests$count.ref.female, tests$count.alt.female), c(tests$count.ref.male, tests$count.alt.male)) ~ 1, betabinomial, trace = T)
+coeff <- Coef(fit)
+alpha <- (1-coeff["rho"])/(coeff["rho"]*(1+(1-coeff["mu"])/coeff["mu"]))
+beta <- alpha*(1-coeff["mu"])/coeff["mu"]
 
- 			data.frame(ID = tmp$ID, count.ref.male = nrefmale, count.alt.male = naltmale, count.ref.female = nreffemale, count.alt.female = naltfemale, stringsAsFactors = F)
-		}
-	  	stopCluster(cl)
-	        pval.fisher.H0 <- ComputePvalueFisher(input.df = pvalH0_out[, c("count.ref.male", "count.alt.male", "count.ref.female", "count.alt.female")])
-	        pvalH0_out$pval.fisher.H0 <- pval.fisher.H0
 
-		saveRDS(pvalH0_out, paste("results/fisher/2017_11_14_simulated_count_H0_from_Bastide_alpha", alphanull, "_biallelic.filt.SNP.same.alt.fisher.RData", sep = ""))	
-	}
-
-	# Choose alpha
-        h <- hist(data.tests$pval.fisher.raw.count, breaks = seq(0, 1, length = 700), plot = F, include.lowest = T, right = T)
-	p.bins <- h$counts/dim(data.tests)[1]
-	res.choose.alpha <- NULL
-	for (alphanull in alpha) {
-		tmp <- readRDS(paste("results/fisher/2017_11_14_simulated_count_H0_from_Bastide_alpha", alphanull, "_biallelic.filt.SNP.same.alt.fisher.RData", sep = ""))
-
-        	htmp <- hist(tmp$pval.fisher.H0, seq(0, 1, length = 700), plot = F, include.lowest = T, right = T)
-		ptest.chisq <- chisq.test(htmp$counts, p = p.bins, correct = F)$p.value
-		ptest.fisher <- fisher.test(cbind(htmp$counts, h$counts), simulate.p.value = T)$p.value
-		ptest.ks <- ks.test(htmp$counts, h$counts)$p.value
-
-		# Quantile-quantile plot of observed vs. simulated P-values for the Fisher’s exact test on allele frequency differentiation
-		png(paste("results/fisher/2017_11_14_qqplot_simulated_vs_obs_pvalues_alpha", alphanull, ".png", sep = ""), w = 850)
-		par(mfrow = c(1,2))
-		qqplot(tmp$pval.fisher.H0, data.tests$pval.fisher.raw.count, main = paste("Quantile-quantile plot of observed vs. simulated\nP-values for the Fisher exact test - alpha=", alphanull, " on 700 bins", sep = ""), xlab = "Simulated null pvalue", ylab = "Observed pvalue")
-		text(0.6, 0.4, paste("pchisq=", ptest.chisq))
-		text(0.6, 0.2, paste("pfisher=", ptest.fisher))
-		text(0.6, 0.0, paste("pks=", ptest.ks))
-		abline(a=0,b=1,lty=2)
-		qqplot(-log10(tmp$pval.fisher.H0), -log10(data.tests$pval.fisher.raw.count), main = paste("Quantile-quantile plot of observed vs. simulated\n-log10(P-values) for the Fisher exact test - alpha=", alphanull, sep = ""), xlab = "Simulated null -log10(pvalue)", ylab = "Observed -log10(pvalue)")
-		abline(a=0,b=1,lty=2)
-		dev.off()
-
-		res.choose.alpha <- rbind(res.choose.alpha, data.frame(alpha = alphanull, beta = (alphanull-alphanull*p.male)/p.male, pchisq_pvalH0_to_obs = ptest.chisq, pfisher_pvalH0_to_obs = ptest.fisher, pks_on_counts_pvalH0_to_obs = ptest.ks, stringsAsFactors = F))
-	}
-	write.table(res.choose.alpha, "results/fisher/2017_11_14_summary_choose_alpha_for_siimulated_count_H0_from_Bastide_biallelic.filt.SNP.same.alt.fisher.txt", sep = "\t", col.names = T, row.names = F, quote = F)
-}
-# Maybe test on 1000bins as well
-
-##### Compute 10 datasets under H0 with alpha = 75
+##### Compute 10 datasets under H0 with alpha set to obtain above
 do.simu.10x <- F
 if(do.simu.10x) {
 	data.tests <- read.csv("results/call_var/2017_11_14_merge.sexe.common.biallelic.filt.SNP.same.alt.fisher.G.txt", sep = "\t", h = T, stringsAsFactors = F)
 	p.male <- mean(data.tests$tot.male)/(mean(data.tests$tot.female)+mean(data.tests$tot.male))
-	alpha.choose <- 75
-	beta.choose <- (alpha.choose-alpha.choose*p.male)/p.male
+	alpha.choose <- alpha
+	beta.choose <- beta
 	ind.ord <- order(data.tests$pval.fisher.raw.count)
 	pval.obs.ord <- data.tests$pval.fisher.raw.count[ind.ord]
 	data.tests.simu <- data.tests[ind.ord, ]
@@ -170,7 +115,6 @@ if(do.simu.10x) {
 	          	tmp <- data.tests[i, ]
 	
 			nref <- tmp$count.ref.female + tmp$count.ref.male
-	                beta.choose <- (alpha.choose-alpha.choose*p.male)/p.male
 	                pmref <- rbeta(1, shape1 = alpha.choose, shape2 = beta.choose)
 			nrefmale <- rbinom(n = 1, size = nref, prob = pmref)
 			nreffemale <- nref-nrefmale
@@ -186,7 +130,7 @@ if(do.simu.10x) {
 	        pval.fisher.H0 <- ComputePvalueFisher(input.df = pvalH0_out[, c("count.ref.male", "count.alt.male", "count.ref.female", "count.alt.female")])
 	        pvalH0_out$pval.fisher.H0 <- pval.fisher.H0
 
-		saveRDS(pvalH0_out, paste("results/fisher/2017_11_15_simulated_#", i, "_count_H0_alpha", alpha.choose, ".RData", sep = ""))	
+		saveRDS(pvalH0_out, paste("results/fisher/2017_11_29_simulated_#", i, "_count_H0_alpha", alpha.choose, ".RData", sep = ""))	
 	}
 
 	pH0.simu <- NULL
@@ -202,20 +146,51 @@ if(do.simu.10x) {
 	write.table(data.tests.simu, "results/call_var/2017_11_14_merge.sexe.common.biallelic.filt.SNP.same.alt.fisher.FDR.G.txt", sep = "\t", col.names = T, row.names = F)
 }	
 
-##### Plot FDR representation by contig with highest nb of significant SNPs to lowest number of significant SNPs
-tests <- read.csv("results/call_var/2017_11_14_merge.sexe.common.biallelic.filt.SNP.same.alt.fisher.FDR.G.txt", sep = "\t", h = T, stringsAsFactors = F)
+##### Zoom on tests output
 size.genome.mbelari <- fread("data/ReferenceGenomes/2017_09_13_Mbelari.sizes.genome", sep = "\t", h = F, stringsAsFactors = F)
+genes.pos <- read.csv("data/ReferenceGenomes/Mesorhabditis_belari_JU2817_v2_genes.bed", h = F, sep = "\t", stringsAsFactors = F)
+all <- NULL
+tests$gene <- ""
+for (ctg in unique(tests$CHROM)) {
+	tmp.pos <- genes.pos[which(genes.pos$V1 == ctg), ]
+	tmp.tests <- tests[which(tests$CHROM == ctg), ]
+	for (i in 1:dim(tmp.tests)[1]){
+		pos <- tmp.tests[i, ]$POS
+		g = tmp.pos[which(tmp.pos$V2<= pos & pos <= tmp.pos$V3), ]$V4
+		if(length(g)>0){
+			tmp.tests[i, ]$gene <- paste(g, collapse = "-")
+		}
+		all <- rbind(all, tmp.tests[i, ])
+	}
+}
+
+tab <- table(tests$qval.Zscore.gsmooth.pseudo.raw<0.05, tests$CHROM)
+tab.signif <- tab[, which(tab[2, ]>0)]
+pdf("results/call_var/2017_11_26_signif_G_prime.pdf")
+barplot(tab.signif[2, ], xlab = "At least 1 SNP signif. on this contig (qval of G')", ylab = "#signif per contig", xaxt = "n",
+main = paste("Output of signif. G' tests (", sum(tab.signif[2, ]), ") per\nSNP at a threshold of 5% per contig (", dim(tab.signif)[2], ")", sep = ""))
+dev.off()
+
+tab <- table(tests$FDR<0.05, tests$CHROM)
+tab.signif <- tab[, which(tab[2, ]>0)]
+pdf("results/call_var/2017_11_26_signif_Fisher.pdf")
+barplot(tab.signif[2, ], xlab = "At least 1 SNP signif. on this contig (FDR for fisher)", ylab = "#signif per contig", xaxt = "n",
+main = paste("Output of signif. fisher tests (", sum(tab.signif[2, ]), ") per\nSNP at a threshold of 5% per contig (", dim(tab.signif)[2], ")", sep = ""))
+dev.off()
+
+
+##### Plot FDR representation by contig with highest nb of significant SNPs to lowest number of significant SNPs
 fisher <- tests[, c("CHROM", "POS", "POS", "REF", "pval.fisher.raw.count", "pval.adjBH.fisher.raw.count", "FDR")]
-tab <- table(tests$FDR<0.01, tests$CHROM)
-tab <- tab[, which(tab[2, ]>0)]
+tab <- table(tests$FDR<0.05, tests$CHROM)
+tab <- tab[1:2, which(tab[2, ]>0)]
 length.tab <- sapply(colnames(tab), function(x) size.genome.mbelari[which(size.genome.mbelari$V1 == x), ]$V2)
 nb.per.bp <- tab[2, ]/length.tab[which(tab[2, ]>0)]
-nb.on.tot <- tab[2, ]/(tab[2,]+tab[1,])
+nb.on.tot <-100*tab[2, ]/(tab[2,]+tab[1,])
 
 ##### Ranking on contigs
-rank.signif.ctgs <- colnames(tab)[rev(order(tab[2, ]))]
-rank.signif.ctgs.per.bp <- colnames(tab)[rev(order(nb.per.bp))]
-rank.signif.ctgs.on.tot <- colnames(tab)[rev(order(nb.on.tot))]
+rank.signif.ctgs <- colnames(tab[1:2, rev(order(tab[2, ]))])
+rank.signif.ctgs.per.bp <- names(rev(order(nb.per.bp)))
+rank.signif.ctgs.on.tot <- names(rev(order(nb.on.tot)))
 
 nb.signif <- 10
 to.plot <- rank.signif.ctgs[1:nb.signif]
@@ -225,15 +200,19 @@ plot <- to.plot
 
 # Prepare input for Manhattan plot
 size.tmp <- size.genome.mbelari[sapply(plot, function(x) which(size.genome.mbelari$V1 == x)), ]
-size.tmp$V1 <- as.factor(size.tmp$V1)
-fisher.tmp <- fisher[unlist(sapply(plot, function(x) which(fisher$CHROM == x))), ]
-fisher.tmp$CHROM <- as.factor(paste("chr", fisher.tmp$CHROM, sep = ""))
+size.tmp$V1 <- factor(size.tmp$V1, levels =  plot)
+fisher.tmp <- fisher[unlist(sapply(plot, function(x) which(fisher$CHROM == x))), c("CHROM", "POS", "POS.1", "REF", "pval.adjBH.fisher.raw.count")]
+fisher.tmp$CHROM <- paste("chr", fisher.tmp$CHROM, sep = "")
+fisher.tmp$CHROM <- factor(fisher.tmp$CHROM, levels = paste("chr", plot, sep = ""))
 fisher.tmp$REF <- 1:dim(fisher.tmp)[1]
+#levels(fisher.tmp$CHROM) <- plot
 
+pdf("results/call_var/2017_11_26_example_Manhattan.pdf")
 plotManhattan(bedfile = fisher.tmp, pvalues = fisher.tmp$FDR,
 col = SushiColors(5),  genome = size.tmp, cex=0.75)
 labelgenome(genome = size.tmp, n = 5, edgeblankfraction = 0.20, cex.axis=.5)
-abline(h = -log10(0.01))
+abline(h = -log10(0.05))
 axis(side=2,las=2,tcl=.2)
 mtext("-log10(P)",side=2,line=1.75,cex=1,font=2)
 mtext("contig",side=1,line=1.75,cex=1,font=2)
+dev.off()
