1916074e50740945dc1f96b54e7bd3f3968e5499
cburny <claire.burny@ens-lyon.fr>
2017-12-14 20:30:25 +0100

    generate_split_contigs_name.sh: remove &&\ at the end of script

--- src/generate_split_contigs_name.sh
+++ src/generate_split_contigs_name.sh
@@ -2,6 +2,6 @@
 
 awk 'BEGIN{-F" "} $0 ~ /^>/ {print substr($1,2)} END {}' 2017_09_12_contigs_name_Ecoli.txt > contigs_short_name_Ecoli.txt &&\
 
-awk 'BEGIN{-F" "} $0 ~ /^>/ {print substr($1,2)} END {}' 2017_09_12_contigs_name_Mbelari.txt > contigs_short_name_Mbelari.txt &&\
+awk 'BEGIN{-F" "} $0 ~ /^>/ {print substr($1,2)} END {}' 2017_09_12_contigs_name_Mbelari.txt > contigs_short_name_Mbelari.txt
 
 
