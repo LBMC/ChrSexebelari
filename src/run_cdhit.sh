661c88f713fdf70a13938929dc95ef2e163c37e2
cburny <claire.burny@ens-lyon.fr>
2017-12-19 21:09:37 +0100

    run_cdhit.sh: add PSMN version of CDHIT indeed called

--- src/run_cdhit.sh
+++ src/run_cdhit.sh
@@ -1,4 +1,20 @@
-# cdhit v4.6.8 
+#$ -S /bin/bash
+### nom du job:
+#$ -N CDHIT
+### file d'attente:
+#$ -q E5-2670deb128*
+### parallel environnement & nslots
+#$ -pe openmp16 16
+### charger l'environnement utilisateur pour SGE
+#$ -cwd
+### exporte les variables d'environnement sur les noeuds d'exécution
+#$ -V
+### change logs folder
+#$ -o /home/cburny/logs
+#$ -e /home/cburny/logs
 
-bin/cdhit/cd-hit-est-2d -i results/mapping/mapped/2017_11_06_MRDR5_trim_Mbelari_Ecoli_unmapped.fa -i2 results/mapping/mapped/2017_11_06_MRDR6_trim_Mbelari_Ecoli_unmapped.fa -o results/mapping/mapped/2017_11_06_cdhit.fa -P 0 -T 4 -M 16000 -c 0.9 -n 9 -l 10 
+source /usr/share/modules/init/bash
+module use /home/cburny/privatemodules_contrib/modulefiles/
+module load CDHIT/4.6.8
 
+cdhit-est-2d -i /scratch/cburny/CDHIT_compare_fasta/2017_11_06_MRDR5_trim_Mbelari_Ecoli_unmapped.fa -i2 /scratch/cburny/CDHIT_compare_fasta/2017_11_06_MRDR6_trim_Mbelari_Ecoli_unmapped.fa -o /scratch/cburny/CDHIT_compare_fasta/2017_11_21_cdhit.fa -n 9 -M 0
