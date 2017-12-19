7c8cb12bac30a62f7774be2f4955b1e23f031c0e
cburny <claire.burny@ens-lyon.fr>
2017-12-19 21:13:47 +0100

    fastq_to_fasta.sh: change directory for the unmapped reads

--- src/fastq_to_fasta.sh
+++ src/fastq_to_fasta.sh
@@ -1,5 +1,5 @@
-# Fastq to fasta
+# Fastq to fasta for CDHIT input
 
-cat results/mapping/mapped/2017_10_30_MRDR6_trim_Mbelari_Ecoli_unmapped.fastq | awk '{if(NR%4==1) {printf(">%s\n",substr($0,2));} else if(NR%4==2) print;}' > results/mapping/mapped/2017_11_06_MRDR6_trim_Mbelari_Ecoli_unmapped.fa &&\
+cat results/mapping/unmapped/2017_10_30_MRDR6_trim_Mbelari_Ecoli_unmapped.fastq | awk '{if(NR%4==1) {printf(">%s\n",substr($0,2));} else if(NR%4==2) print;}' > results/mapping/unmapped/2017_11_06_MRDR6_trim_Mbelari_Ecoli_unmapped.fa &&\
 
-cat results/mapping/mapped/2017_10_30_MRDR5_trim_Mbelari_Ecoli_unmapped.fastq | awk '{if(NR%4==1) {printf(">%s\n",substr($0,2));} else if(NR%4==2) print;}' > results/mapping/mapped/2017_11_06_MRDR5_trim_Mbelari_Ecoli_unmapped.fa
+cat results/mapping/unmapped/2017_10_30_MRDR5_trim_Mbelari_Ecoli_unmapped.fastq | awk '{if(NR%4==1) {printf(">%s\n",substr($0,2));} else if(NR%4==2) print;}' > results/mapping/unmapped/2017_11_06_MRDR5_trim_Mbelari_Ecoli_unmapped.fa
