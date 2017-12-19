021d7204cfb2162e5772a58ecf701f063115d5c0
cburny <claire.burny@ens-lyon.fr>
2017-12-19 18:02:56 +0100

    QC.sh and concatenate_belari_coli.sh : add comments

--- src/QC.sh
+++ src/QC.sh
@@ -1,3 +1,4 @@
+# QC and raw data processing
 # done on local computer 
 
 bin/nextflow src/pipe/quality_control.nf -c src/nextflow.config -profile quality_control --fastq_files "data/fastq/*6*" --cpu 4 --fastqc "/usr/local/bin/FastQC/fastqc" --paired true --trimmer "cutadapt" --quality_threshold 20 --pigz_version "2.3" --do_adapter_removal false
--- src/concatenate_belari_coli.sh
+++ src/concatenate_belari_coli.sh
@@ -1,2 +1,4 @@
-##### Concatenation was done on uncompressed fasta
+# Concatenate E. coli and M. belari fasta. This was done on uncompressed fasta files
+# done on local computer 
+
 cat data/ReferenceGenomes/GCA_000176815.1_ASM17681v1_genomic.fna data/ReferenceGenomes/Mesorhabditis_belari_JU2817_v2.scaffolds.fa.repeatmasker.masked | gzip > data/ReferenceGenomes/combined_belari_coli.fasta.gz
