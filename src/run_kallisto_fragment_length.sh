eae8b0f929dc614d5a5976537d58a6550397abd5
cburny <claire.burny@ens-lyon.fr>
2017-12-19 18:16:27 +0100

    run_kallisto_fragment_length.sh: add comments

--- src/run_kallisto_fragment_length.sh
+++ src/run_kallisto_fragment_length.sh
@@ -1,4 +1,6 @@
-##### kallisto version 0.43.1
-bin/kallisto index -i data/ReferenceGenomes/m_belari_kall_index data/ReferenceGenomes/mesorhabditis_belari_assembly.2.0.SOFTMASKED.fa
+# Estimate fragment length distribution on processed reads. This was done on uncompressed ref fasta file.
+# done on local computer 
+
+bin/kallisto index -i data/ReferenceGenomes/m_belari_kall_index data/ReferenceGenomes/mesorhabditis_belari_assembly.2.0.SOFTMASKED.fa &&\
 
 bin/kallisto quant -i data/ReferenceGenomes/m_belari_kall_index -o results/kallisto_fragmentlength results/quality_control/trimming/2017_08_08_MRDR5_trim_R1.fastq.gz results/quality_control/trimming/2017_08_08_MRDR5_trim_R2.fastq.gz results/quality_control/trimming/2017_08_08_MRDR6_trim_R1.fastq.gz results/quality_control/trimming/2017_08_08_MRDR6_trim_R2.fastq.gz
