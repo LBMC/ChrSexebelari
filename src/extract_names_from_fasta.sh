6f9a182cec32dd130f7708054dc6f18e18a97cca
cburny <claire.burny@ens-lyon.fr>
2017-12-19 19:21:54 +0100

    extract_names_from_fasta.sh and generate_split_contigs_name.sh: add comments

--- src/extract_names_from_fasta.sh
+++ src/extract_names_from_fasta.sh
@@ -1,4 +1,7 @@
+# Extract names of sequences from a fasta.
+# done on local computer 
 # $1 is input file and $2 is output for saving sequence name from fasta
+
 awk 'BEGIN{-F""} 
 $0 ~ /^>/ {print $0} 
 END {print "name of seq"}' $1 > $2
--- src/generate_split_contigs_name.sh
+++ src/generate_split_contigs_name.sh
@@ -1,7 +1,8 @@
-##### Quantify reads either mapped on M. belari or E. coli
+# Extract first element of names of sequences from a fasta.
+# done on local computer 
 
-awk 'BEGIN{-F" "} $0 ~ /^>/ {print substr($1,2)} END {}' 2017_09_12_contigs_name_Ecoli.txt > contigs_short_name_Ecoli.txt &&\
+awk 'BEGIN{-F" "} $0 ~ /^>/ {print substr($1,2)} END {}' 2017_09_12_contigs_name_Ecoli.txt > 2017_09_12_contigs_short_name_Ecoli.txt &&\
 
-awk 'BEGIN{-F" "} $0 ~ /^>/ {print substr($1,2)} END {}' 2017_09_12_contigs_name_Mbelari.txt > contigs_short_name_Mbelari.txt
+awk 'BEGIN{-F" "} $0 ~ /^>/ {print substr($1,2)} END {}' 2017_09_12_contigs_name_Mbelari.txt > 2017_09_12_contigs_short_name_Mbelari.txt
 
 
