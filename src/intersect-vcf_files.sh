3594881660d183664791cf510e74e460fff1433b
cburny <claire.burny@ens-lyon.fr>
2017-12-20 15:20:36 +0100

    intersect-vcf_files.sh: add comments

--- src/intersect-vcf_files.sh
+++ src/intersect-vcf_files.sh
@@ -12,7 +12,7 @@ bcftools index results/call_var/2017_09_21_MRDR5_trim_Mbelari_mapped_rmdup_rg_re
 # Make intersection: this generates 2 vcf files: 
 # - 0000.vcf which represents variants in MRDR6 (1st sample) and not in MRDR5 (2nd sample)
 # - 0002.vcf which represents variants present in both pools
-bcftools isec results/call_var/MRDR6_trim_Mbelari_mapped_rmdup_rg_realign_indels.vcf.gz results/call_var/MRDR5_trim_Mbelari_mapped_rmdup_rg_realign_indels.vcf.gz -p results/call_var/inter_female_male_realign_indels -w 1 &&\
+bcftools isec results/call_var/2017_09_21_MRDR6_trim_Mbelari_mapped_rmdup_rg_realign_indels.vcf.gz results/call_var/2017_09_21_MRDR5_trim_Mbelari_mapped_rmdup_rg_realign_indels.vcf.gz -p results/call_var/inter_female_male_realign_indels -w 1 &&\
 
 # Make summary with rtg
 bin/rtg vcfstats results/call_var/inter_female_male_realign_indels/0000.vcf > results/call_var/inter_female_male_realign_indels/summary_in_MRDR6_not_in_MRDR5_0000.txt &&\
