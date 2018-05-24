462f524eadd4224df3e17e7b071839208e079155
Lilia <lilia.estrada-virrueta@ens-lyon.fr>
2018-05-24 17:04:10 +0200

    Trying to run DBGOLC but failing in the third/last step

--- src/hybrid_test/2018-05-22-Hybrid_test_MBELA01182.sh
+++ src/hybrid_test/2018-05-22-Hybrid_test_MBELA01182.sh
@@ -1,1 +1,14 @@
-./SparseAssembler g 15 k 51 LD 1 GS 100000 NodeCovTh 1 EdgeCovTh 1 p1 ~/Documents/stage_mbelari/data/sample/MR_350_mapped_to_Mbelari-01182_1.fastq p2 ~/Documents/stage_mbelari/data/sample/MR_350_mapped_to_Mbelari-01182_2.fastq  &
+~/Programs/DBG2OLC-master/compiled/SparseAssembler g 15 k 51 LD 0 GS 100000 NodeCovTh 1 EdgeCovTh 1 p1 ~/Documents/stage_mbelari/data/sample/MR_350_mapped_to_Mbelari-01182_1.fastq p2 ~/Documents/stage_mbelari/data/sample/MR_350_mapped_to_Mbelari-01182_2.fastq 
+
+~/Programs/DBG2OLC-master/compiled/DBG2OLC k 17 MinLen 500 AdaptiveTh 0.005 KmerCovTh 4 MinOverlap 20 RemoveChimera 0 Contigs Contigs.txt f ~/Documents/stage_mbelari/data/sample/MBELA01182subset.fastq
+
+seqtk seq -a ~/Documents/stage_mbelari/data/sample/MBELA01182subset.fastq > MBELA01182subset.fasta
+
+cat Contigs.txt MBELA01182subset.fasta > ctg_pb.fasta
+
+ulimit -n unlimited
+
+sh ~/Programs/DBG2OLC-master/utility/split_and_run_sparc.sh backbone_raw.fasta DBG2OLC_Consensus_info.txt ctg_pb.fasta consensus_dir 2 >cns_log.txt
+
+sh ./split_and_run_sparc.sh ~/Documents/stage_mbelari/results/hybrid_assembly/backbone_raw.fasta ~/Documents/stage_mbelari/results/hybrid_assembly/DBG2OLC_Consensus_info.txt ~/Documents/stage_mbelari/results/hybrid_assembly/ctg_pb.fasta ~/Documents/stage_mbelari/results/hybrid_assembly/consensus_dir 2 > ~/Documents/stage_mbelari/results/hybrid_assembly/cns_log.txt
+
