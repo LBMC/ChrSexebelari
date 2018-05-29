26bf75868e02b2f6fe8b1dee525ed3d425dba4a1
Lilia <lilia.estrada-virrueta@ens-lyon.fr>
2018-05-29 11:42:30 +0200

    Constructing a new nanopore reads subset by using BWA-MEM

--- src/data_transformations/2018-05-29-Nanopore_reads_subset_construction.sh
+++ src/data_transformations/2018-05-29-Nanopore_reads_subset_construction.sh
@@ -1,9 +1,19 @@
+#bwa index ~/Documents/stage_mbelari/data/sample/MBELA01182_RNAP2.fa 
+
+#bwa mem -x ont2d ~/Documents/stage_mbelari/data/sample/MBELA01182_RNAP2.fa ~/Documents/stage_mbelari/data/sample/MBELA01182subset.fastq > ~/Documents/stage_mbelari/results/nanopore_subset/2018-05-29-Nanopore_MBELA01182_subset.bam
+
+#samtools sort ~/Documents/stage_mbelari/results/nanopore_subset/2018-05-29-Nanopore_MBELA01182_subset.bam -o ~/Documents/stage_mbelari/results/nanopore_subset/2018-05-29-Nanopore_MBELA01182_subset.sorted.bam
+
+#samtools index ~/Documents/stage_mbelari/results/nanopore_subset/2018-05-29-Nanopore_MBELA01182_subset.sorted.bam
+
+#samtools faidx ~/Documents/stage_mbelari/data/sample/MBELA01182_RNAP2.fa
+
+
+
 bwa index ~/Documents/stage_mbelari/data/sample/MBELA01182_RNAP2.fa 
 
-bwa mem -x ont2d ~/Documents/stage_mbelari/data/sample/MBELA01182_RNAP2.fa ~/Documents/stage_mbelari/data/sample/MBELA01182subset.fastq > ~/Documents/stage_mbelari/results/nanopore_against_ecoli_genome/2018-05-18-Nanopore_MBELA01182_subset.bam
+bwa mem -x ont2d ~/Documents/stage_mbelari/data/sample/MBELA01182_RNAP2.fa ~/Documents/stage_mbelari/results/nanopore_against_ecoli_genome/2018-05-24-Nanopore_all_reads_best_qual_wo_ecoli.fastq > ~/Documents/stage_mbelari/results/nanopore_subset/2018-05-29-Nanopore_MBELA01182_subset_2.bam
 
-samtools sort ~/Documents/stage_mbelari/results/illumina_subset/2018-05-28-Illumina_subset_MBELA01182.bam -o ~/Documents/stage_mbelari/results/illumina_subset/2018-05-28-Illumina_subset_MBELA01182.sorted.bam
+samtools view -F4 ~/Documents/stage_mbelari/results/nanopore_subset/2018-05-29-Nanopore_MBELA01182_subset_2.bam > ~/Documents/stage_mbelari/results/nanopore_subset/2018-05-29-Nanopore_MBELA01182_subset_2.mapped.bam
 
-samtools index ~/Documents/stage_mbelari/results/illumina_subset/2018-05-28-Illumina_subset_MBELA01182.sorted.bam
-
-samtools faidx ~/Documents/stage_mbelari/data/sample/MBELA01182_RNAP2.fa
+cut -f1 ~/Documents/stage_mbelari/results/nanopore_subset/2018-05-29-Nanopore_MBELA01182_subset_2.mapped.bam | sort | uniq > ~/Documents/stage_mbelari/results/nanopore_subset/2018-05-29-Nanopore_MBELA01182_subset_2_mapped.lst
