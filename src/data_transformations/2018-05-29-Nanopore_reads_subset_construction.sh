70b905c443af642e7997058e16d40af7e8b9d1a2
Lilia <lilia.estrada-virrueta@ens-lyon.fr>
2018-05-29 14:26:19 +0200

    New nanopore subset obtained by aligning with BWA-MEM the nanopore reads vs the contig MBELA01182

--- src/data_transformations/2018-05-29-Nanopore_reads_subset_construction.sh
+++ src/data_transformations/2018-05-29-Nanopore_reads_subset_construction.sh
@@ -17,3 +17,10 @@ bwa mem -x ont2d ~/Documents/stage_mbelari/data/sample/MBELA01182_RNAP2.fa ~/Doc
 samtools view -F4 ~/Documents/stage_mbelari/results/nanopore_subset/2018-05-29-Nanopore_MBELA01182_subset_2.bam > ~/Documents/stage_mbelari/results/nanopore_subset/2018-05-29-Nanopore_MBELA01182_subset_2.mapped.bam
 
 cut -f1 ~/Documents/stage_mbelari/results/nanopore_subset/2018-05-29-Nanopore_MBELA01182_subset_2.mapped.bam | sort | uniq > ~/Documents/stage_mbelari/results/nanopore_subset/2018-05-29-Nanopore_MBELA01182_subset_2_mapped.lst
+
+seqtk subseq ~/Documents/stage_mbelari/results/nanopore_against_ecoli_genome/2018-05-24-Nanopore_all_reads_best_qual_wo_ecoli.fastq ~/Documents/stage_mbelari/results/nanopore_subset/2018-05-29-Nanopore_MBELA01182_subset_2_mapped.lst > ~/Documents/stage_mbelari/results/nanopore_subset/2018-05-29-Nanopore_MBELA01182_subset_2.fq
+
+samtools sort ~/Documents/stage_mbelari/results/nanopore_subset/2018-05-29-Nanopore_MBELA01182_subset_2.bam -o ~/Documents/stage_mbelari/results/nanopore_subset/2018-05-29-Nanopore_MBELA01182_subset_2.sorted.bam
+
+samtools index ~/Documents/stage_mbelari/results/nanopore_subset/2018-05-29-Nanopore_MBELA01182_subset_2.sorted.bam
+
