5eb88562d60cf809c729e8029f5922f7c6ce9ca4
Lilia <lilia.estrada-virrueta@ens-lyon.fr>
2018-05-30 14:43:15 +0200

    hybrid assembly DBG2OLC pipeline the posibility of using two pairend files was added but commented

--- src/hybrid_test/2018-05-28-Hybrid_test-DBG2OLC.sh
+++ src/hybrid_test/2018-05-28-Hybrid_test-DBG2OLC.sh
@@ -2,21 +2,24 @@
 
 illumina_fq_1=$1
 illumina_fq_2=$2
-nanopore_reads_fq=$3
+genome_size=$3
+nanopore_reads_fa=$4
 
-chunk=`basename ${nanopore_reads_fq} .fastq`
+~/Programs/DBG2OLC-master/compiled/SparseAssembler g 15 k 51 LD 0 GS ${genome_size} NodeCovTh 2 EdgeCovTh 1 p1 ${illumina_fq_1} p2 ${illumina_fq_2}
 
-seqtk seq -a ${nanopore_reads_fq} > ${chunk}.fasta
+#mv Contigs.txt Contigs_350.txt
 
-nanopore_reads_fa= ${chunk}.fasta
+#~/Programs/DBG2OLC-master/compiled/SparseAssembler g 15 k 51 LD 0 GS ${genome_size} NodeCovTh 2 EdgeCovTh 1 p1 ${illumina_fq_1} p2 ${illumina_fq_2}
 
-~/Programs/DBG2OLC-master/compiled/SparseAssembler g 15 k 51 LD 0 GS 100000 NodeCovTh 2 EdgeCovTh 1 p1 ${illumina_fq_1} p2 ${illumina_fq_2}
+#mv Contigs.txt Contigs_550.txt
+
+#sed -i 's/Contig/Contig_350/g' Contigs_350.txt
+#sed -i 's/Contig/Contig_550/g' Contigs_550.txt
+#cat Contigs_350.txt Contigs_550.txt > Contigs.txt
 
 #Nanopore files and Contigs.txt file from previous step
 
-~/Programs/DBG2OLC-master/compiled/DBG2OLC k 17 MinLen 250 AdaptiveTh 0.001 KmerCovTh 3 MinOverlap 15 RemoveChimera 1 Contigs Contigs.txt f ${nanopore_reads_fq}
-
-#Convert fastq nanopore reads to fasta
+~/Programs/DBG2OLC-master/compiled/DBG2OLC k 17 MinLen 250 AdaptiveTh 0.001 KmerCovTh 3 MinOverlap 15 RemoveChimera 1 Contigs Contigs.txt f ${nanopore_reads_fa}
 
 cat Contigs.txt ${nanopore_reads_fa} > ctg_pb.fasta
 
@@ -25,4 +28,3 @@ cat Contigs.txt ${nanopore_reads_fa} > ctg_pb.fasta
 #Consensus step
 
 sh ~/Programs/DBG2OLC-master/utility/split_and_run_sparc_test.sh backbone_raw.fasta DBG2OLC_Consensus_info.txt ctg_pb.fasta consensus_dir 5 > cns_log.txt
-s	
