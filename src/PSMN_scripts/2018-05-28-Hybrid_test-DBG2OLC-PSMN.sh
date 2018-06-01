f5ed1eb61805959e7cebf5d738bcfe48a7659223
lestrada <lilia.estrada-virrueta@ens-lyon.fr>
2018-06-01 16:05:32 +0200

    the final script used for the hybrid assembly 1st test

--- src/PSMN_scripts/2018-05-28-Hybrid_test-DBG2OLC-PSMN.sh
+++ src/PSMN_scripts/2018-05-28-Hybrid_test-DBG2OLC-PSMN.sh
@@ -1,25 +1,18 @@
 #!/usr/bin/env bash
 
-illumina_fq_1=$1
-illumina_fq_2=$2
-genome_size=$3
-nanopore_reads_fa=$4
+illumina_fq_1_1=$1
+illumina_fq_1_2=$2
+illumina_fq_2_1=$3
+illumina_fq_2_2=$4
+genome_size=$5
+nanopore_reads_fa=$6
 
-~/stage_mbelari/bin/DBG2OLC-master/compiled/SparseAssembler g 15 k 51 LD 0 GS ${genome_size} NodeCovTh 2 EdgeCovTh 1 p1 ${illumina_fq_1} p2 ${illumina_fq_2}
+#/scratch/lestrada/stage_mbelari/bin/DBG2OLC-master/compiled/SparseAssembler g 15 k 51 LD 0 GS ${genome_size} NodeCovTh 2 EdgeCovTh 1 p1 ${illumina_fq_1_1} p2 ${illumina_fq_1_2} p1 ${illumina_fq_2_1} p2 ${illumina_fq_2_2}
 
-#mv Contigs.txt Contigs_350.txt
-
-#~/stage_mbelari/bin/DBG2OLC-master/compiled/SparseAssembler g 15 k 51 LD 0 GS ${genome_size} NodeCovTh 2 EdgeCovTh 1 p1 ${illumina_fq_1} p2 ${illumina_fq_2}
-
-#mv Contigs.txt Contigs_550.txt
-
-#sed -i 's/Contig/Contig_350/g' Contigs_350.txt
-#sed -i 's/Contig/Contig_550/g' Contigs_550.txt
-#cat Contigs_350.txt Contigs_550.txt > Contigs.txt
 
 #Nanopore files and Contigs.txt file from previous step
 
-~/stage_mbelari/bin/DBG2OLC-master/compiled/DBG2OLC k 17 MinLen 250 AdaptiveTh 0.001 KmerCovTh 3 MinOverlap 15 RemoveChimera 1 Contigs Contigs.txt f ${nanopore_reads_fa}
+/scratch/lestrada/stage_mbelari/bin/DBG2OLC-master/compiled/DBG2OLC k 17 MinLen 250 AdaptiveTh 0.001 KmerCovTh 3 MinOverlap 15 RemoveChimera 1 Contigs Contigs.txt f ${nanopore_reads_fa}
 
 cat Contigs.txt ${nanopore_reads_fa} > ctg_pb.fasta
 
@@ -27,4 +20,4 @@ cat Contigs.txt ${nanopore_reads_fa} > ctg_pb.fasta
 
 #Consensus step
 
-sh ~/stage_mbelari/bin/DBG2OLC-master/utility/split_and_run_sparc_test.sh backbone_raw.fasta DBG2OLC_Consensus_info.txt ctg_pb.fasta consensus_dir 5 > cns_log.txt
+sh /scratch/lestrada/stage_mbelari/bin/DBG2OLC-master/utility/split_and_run_sparc_test.sh backbone_raw.fasta DBG2OLC_Consensus_info.txt ctg_pb.fasta consensus_dir 5 > cns_log.txt
