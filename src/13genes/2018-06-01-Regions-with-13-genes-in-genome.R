a73634fc58e748f08c160367ae709b389dfc85da
Lilia <lilia.estrada-virrueta@ens-lyon.fr>
2018-06-13 10:23:41 +0200

    Preparing figures

--- src/13genes/2018-06-01-Regions-with-13-genes-in-genome.R
+++ src/13genes/2018-06-01-Regions-with-13-genes-in-genome.R
@@ -73,10 +73,75 @@ for (l in (1:length(genes_tested))){
 	}
 
 count_genes_in_scaffolds <- table(file[,1],file[,2])
-write.csv(count_genes_in_scaffolds,'Genes_in_scaffolds.csv')
+#write.csv(count_genes_in_scaffolds,'Genes_in_scaffolds.csv')
 
 Large_contig <- file[file[,2]=='Backbone_495',]
 Check <- c(1,1,1,0,1,0,1)
 Large_contig <- data.frame(Large_contig,Check)
 Large_contig[Check==1,]
+
+genes_13 <- read.fasta('~/Documents/stage_mbelari/data/raw/2018-05-18-Complete_13_genes.fasta')
+cds_names <- sub('\\.t[0-9]*','',cds_names)
+getcds <- match(getName(genes_13)[-1],cds_names)
+
+file <- read.table('~/Documents/stage_mbelari/results/13genes/2018-06-01-13genes_in_hybrid_genome.txt',header=F)
+sense <- file[,9]>file[,10]
+file <- data.frame(file,sense)
+count_genes_in_scaffolds <- table(file[,1],file[,2])
+for (l in 1:length(table(file[,1]))){
+	subfile <- file[file[,1]==rownames(count_genes_in_scaffolds)[l],]
+	temp <- subfile[,12]
+	count_genes_in_scaffolds[subfile[1,1],subfile[,2]] <- subfile[,12]
+	repeated <- which(table(names(temp))>1)
+	if(length(repeated)!=0){
+	count_genes_in_scaffolds[subfile[1,1],names(repeated)] <- max(subfile[subfile[2]==names(repeated),12])
+	}
+}
+final_count <- apply(count_genes_in_scaffolds,1,function(x){x/max(x)})
+library(RColorBrewer)
+#my_col=colorRampPalette(brewer.pal(8, "Blues"))(10)
+my_col=colorRampPalette(c("white","purple"))(10)
+heatmap(final_count, col= my_col)
+library(gplots)
+heatmap.2(final_count, col= my_col, trace='none', Rowv=FALSE, Colv=FALSE)
+png('Genes_in_contigs.png', width=1000,height=1000)
+heatmap.2(final_count, col= my_col, trace='none', Rowv=FALSE, Colv=FALSE,keysize=1,key.title=NA, key.xlab='Alignment score', cexRow=0.9, cexCol=1.3, margins=c(5.5,6.8))
+dev.off()
+
+library(genoPlotR)
+
+dna_seg_list <- list()
+
+#drawcontigs <- names(which(table(file[,2])>1))
+drawcontigs <- c('Backbone_495','Backbone_1072','Backbone_183')
+#drawcontigs <- c('Backbone_1052','Backbone_1092','Backbone_38')
+annot_list <- list()
+for (l in 1:length(drawcontigs)){
+	subfilea <- file[file[,2]==drawcontigs[l],]
+	gene_names <- as.character(subfilea[,1])
+	starts <- subfilea[,9]
+	ends <- subfilea[,10]
+	strands <- as.numeric(subfilea[,13])
+	strands[strands==0] <- -1
+	cols <- colorRampPalette(c("darksalmon","firebrick4"))(length(gene_names))
+	df <- data.frame(name=gene_names,start=starts,end=ends,strand=strands,col=cols)
+	dna_seg_list[[l]] <- as.dna_seg(df, fill=cols, lty=2)
+	annot_list[[l]] <- annotation(middle(dna_seg_list[[l]]), text=gene_names, rot=0, col=dna_seg_list[[l]]$col)
+}
+
+col <- c('brown3','deepskyblue3','forestgreen','darkslateblue','coral2','darkgoldenrod1','darkolivegreen2','antiquewhite3','cornflowerblue')
+col1 <- col[c(1,1,2,2,3,3,4)]
+col2 <- col[c(5,1,2,4)]
+col3 <- col[c(6,7,8)]
+dna_seg_list[[1]]$col <-col1
+dna_seg_list[[2]]$col <-col2
+dna_seg_list[[3]]$col <-col3
+dna_seg_list[[1]]$fill <-col1
+dna_seg_list[[2]]$fill <-col2
+dna_seg_list[[3]]$fill <-col3
+png('Contigs.png', width=800, height=300)
+#png('Contigs.png')
+#pdf('Contigs.pdf', width=1000, height=1000)
+plot_gene_map(dna_seg_list, dna_seg_labels=drawcontigs, dna_seg_labes_cex=1.2,annotations=annot_list, annotations.cex=1.5)
+dev.off()
 
