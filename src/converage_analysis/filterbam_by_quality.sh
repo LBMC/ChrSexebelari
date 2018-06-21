3d6509d5048ab2db01e55a7a638492fa327e6b4e
Lilia <lilia.estrada-virrueta@ens-lyon.fr>
2018-06-21 14:37:20 +0200

    change name

--- src/converage_analysis/filterbam_by_quality.sh
+++
@@ -1,4 +0,0 @@
-samtools view -bq 5 2018-06-12-MRDR5_vs_Hybrid_assembly.sorted.bam > 2018-06-12-MRDR5_vs_Hybrid_assembly.sorted.filtered.bam
-
-samtools view -bq 5 2018-06-12-MRDR6_vs_Hybrid_assembly.sorted.bam > 2018-06-12-MRDR6_vs_Hybrid_assembly.sorted.filtered.bam
-
