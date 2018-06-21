8277db2208dd73ea4e2415c11abba6c862101bb1
Lilia <lilia.estrada-virrueta@ens-lyon.fr>
2018-06-21 11:13:11 +0200

    Look for high FC genes

---
+++ src/13genes/Genes_female_male.R
@@ -0,0 +1,18 @@
+library(seqinr)
+
+source('~/Documents/stage_mbelari/src/13genes/Functions.R')
+
+FCgenes <- read.table('2017_12_06_FC_normalized_coverage_at_gene.txt', head=T, sep='\t')
+male_absent <- FCgenes[FCgenes$missing.sexe=='male',]
+genes_male_absent <- sub('MBELA\\.','',as.character(male_absent[,1]))
+
+#getCdsV2(genes_male_absent,'List_genes_absent_in_males.txt')
+female_absent <- FCgenes[FCgenes$missing.sexe=='female',]
+
+top_50_fem_abs <- FCgenes[order(FCgenes$log2.norm.FC)[1:50],]
+genes_top_50_fem_abs <- sub('MBELA\\.','',as.character(top_50_fem_abs[,1]))
+
+getCdsV2(genes_top_50_fem_abs,'2018-06-20-Top_50_genes_fem_abs.fasta')
+
+Blast_results <- read.table('2018-06-20-Genes_absent_females.txt',head=FALSE)
+genes_in_backbones <- table(Blast_results[,1],Blast_results[,2])
