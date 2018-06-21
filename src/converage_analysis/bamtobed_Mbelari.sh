bb6277612738bf08fc3b85f49523db050293d887
Lilia <lilia.estrada-virrueta@ens-lyon.fr>
2018-06-21 11:21:09 +0200

    run for males

--- src/converage_analysis/bamtobed_Mbelari.sh
+++ src/converage_analysis/bamtobed_Mbelari.sh
@@ -1,8 +1,8 @@
 #The bam files were filtered with 
 
-INPUTcov=~/Documents/stage_mbelari/results/13genes/2018-06-12-MRDR5_vs_Hybrid_assembly.sorted.filtered.bam
+INPUTcov=~/Documents/stage_mbelari/results/13genes/2018-06-12-MRDR6_vs_Hybrid_assembly.sorted.filtered.bam
 GENOMEsizes=~/Documents/stage_mbelari/results/coverage_analysis/2018-06-21-Mbelari_hybrid_genome_sizes.txt
-OUTPUTcov=~/Documents/stage_mbelari/results/coverage_analysis/2018_06_21_MRDR5_vs_Hybrid_assembly.sorted.filtered.bed
+OUTPUTcov=~/Documents/stage_mbelari/results/coverage_analysis/2018_06_21_MRDR6_vs_Hybrid_assembly.sorted.filtered.bed
 
 bedtools genomecov -d -ibam $INPUTcov -g $GENOMEsizes > $OUTPUTcov
 
