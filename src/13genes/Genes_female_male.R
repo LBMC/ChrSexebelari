8a91581ebb4b70369fff646f601775216c0c0b36
Lilia <lilia.estrada-virrueta@ens-lyon.fr>
2018-06-26 11:32:21 +0200

    Look for significant genes

--- src/13genes/Genes_female_male.R
+++ src/13genes/Genes_female_male.R
@@ -1,6 +1,6 @@
 library(seqinr)
 
-source('~/Documents/stage_mbelari/src/13genes/Functions.R')
+source('~/Documents/stage_mbelari/src/Functions.R')
 
 FCgenes <- read.table('2017_12_06_FC_normalized_coverage_at_gene.txt', head=T, sep='\t')
 male_absent <- FCgenes[FCgenes$missing.sexe=='male',]
@@ -14,5 +14,9 @@ genes_top_50_fem_abs <- sub('MBELA\\.','',as.character(top_50_fem_abs[,1]))
 
 getCdsV2(genes_top_50_fem_abs,'2018-06-20-Top_50_genes_fem_abs.fasta')
 
-Blast_results <- read.table('2018-06-20-Genes_absent_females.txt',head=FALSE)
+Blast_results <- read.table('~/Documents/stage_mbelari/results/13genes/version2/2018-06-20-Genes_absent_females.txt',head=FALSE)
 genes_in_backbones <- table(Blast_results[,1],Blast_results[,2])
+
+genes_in_backbones[genes_in_backbones>=1]<-1
+apply(genes_in_backbones,2,sum)
+
