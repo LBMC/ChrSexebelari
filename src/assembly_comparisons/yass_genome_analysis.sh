9929f8d1c8789bb0169fb65c63a633ce48f6362c
Lilia <lilia.estrada-virrueta@ens-lyon.fr>
2018-06-26 13:16:41 +0200

    run yass alignment

---
+++ src/assembly_comparisons/yass_genome_analysis.sh
@@ -0,0 +1,4 @@
+union -filter ~/Documents/stage_mbelari/results/hybrid_test/DBG2OLC/2018-06-01/final_assembly.fasta > 2018-06-25-Merged_hybrid_assembly.fasta
+
+yass ~/Documents/stage_mbelari/results/genome_analysis/2018-06-25-Merged_hybrid_assembly.fasta -o 2018-06-25-genome_analysis
+
