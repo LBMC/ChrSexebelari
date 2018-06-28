1d603f9a471f8542c28d4400123d7605ad2e9261
Lilia <lilia.estrada-virrueta@ens-lyon.fr>
2018-06-28 11:15:22 +0200

    moved

--- src/assembly_comparisons/yass_genome_analysis.sh
+++
@@ -1,4 +0,0 @@
-union -filter ~/Documents/stage_mbelari/results/hybrid_test/DBG2OLC/2018-06-01/final_assembly.fasta > 2018-06-25-Merged_hybrid_assembly.fasta
-
-yass ~/Documents/stage_mbelari/results/genome_analysis/2018-06-25-Merged_hybrid_assembly.fasta -o 2018-06-25-genome_analysis
-
