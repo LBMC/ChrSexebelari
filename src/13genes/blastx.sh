96d7cdf613f0a4a199f489259bac11b45bb769c7
Lilia <lilia.estrada-virrueta@ens-lyon.fr>
2018-06-28 11:16:59 +0200

    blasxt script

---
+++ src/13genes/blastx.sh
@@ -0,0 +1,1 @@
+blastx -query ~/Documents/stage_mbelari/results/13genes/version2/2018-06-20-Top_50_genes_fem_abs.fasta -db ~/Programs/ncbi-blast-2.7.1+/blastdb/nr -out 2018-06-27-Blastx-top-50genes.txt -outfmt 6
