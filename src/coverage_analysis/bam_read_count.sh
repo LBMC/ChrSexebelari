cd320f3cecb6a684192bf539c4422357eae8115a
Lilia <lilia.estrada-virrueta@ens-lyon.fr>
2018-07-05 17:09:53 +0200

    input changed

--- src/coverage_analysis/bam_read_count.sh
+++ src/coverage_analysis/bam_read_count.sh
@@ -1,4 +1,4 @@
-INPUTbam=~/Documents/stage_mbelari/results/13genes/2018-06-12-MRDR5_vs_Hybrid_assembly.sorted.filtered.bam
-OUTPUT=~/Documents/stage_mbelari/results/coverage_analysis/2018_06_21_MRDR5_vs_Hybrid_assembly.sorted.filtered.counts
+INPUTbam=~/Documents/stage_mbelari/results/sex_chromosome/2018-06-29-redundans_assembly_2_MRDR6.sorted.bam
+OUTPUT=~/Documents/stage_mbelari/results/coverage_analysis/2018_06_29_MRDR6_vs_redundans_assembly.sorted.filtered.counts
 
 samtools idxstats ${INPUTbam} > ${OUTPUT}
