912df5871d76a6ea144f0e0f17df913012cbe2fc
Lilia <lilia.estrada-virrueta@ens-lyon.fr>
2018-07-26 17:29:18 +0200

    Claires scripts

--- src/coverage_analysis/bam_read_count.sh
+++ src/coverage_analysis/bam_read_count.sh
@@ -1,4 +1,4 @@
-INPUTbam=~/Documents/stage_mbelari/results/sex_chromosome/2018-06-29-redundans_assembly_2_MRDR6.sorted.bam
-OUTPUT=~/Documents/stage_mbelari/results/coverage_analysis/2018_06_29_MRDR6_vs_redundans_assembly.sorted.filtered.counts
+INPUTbam=results/coverage_analysis/2018-06-12-MRDR6_vs_Hybrid_assembly.sorted.filtered.bam
+OUTPUT=results/coverage_analysis/2018-06-12-MRDR6_vs_Hybrid_assembly.sorted.filtered.counts
 
 samtools idxstats ${INPUTbam} > ${OUTPUT}
---
+++ src/coverage_analysis/format_bed.R
@@ -0,0 +1,11 @@
+require(data.table)
+
+# Convert 0-based bed obtained after gff3 to bed transformation in 1-based
+# systeme coordinates
+bed <- fread("~/Documents/stage_mbelari/results/annotation/coverage_analysis/Mesorhabditis_belari_JU2817_hybrid_assembly_genes.bed", sep = "\t", 
+  h = F, stringsAsFactors = F)
+bed$V2 <- bed$V2 + 1
+bed$V3 <- bed$V3 + 1
+write.table(bed, "~/Documents/stage_mbelari/results/annotation/coverage_analysis/Mesorhabditis_belari_JU2817_hybrid_assembly_genes_1based.bed", 
+  sep = "\t", col.names = F, row.names = F, quote = F)
+
