5266317942555a1f966c9354368ba1db584c9a0d
Lilia <lilia.estrada-virrueta@ens-lyon.fr>
2018-07-26 17:32:07 +0200

    change path

--- src/13genes/Blast-vs-draft-hybrid-genome.sh
+++ src/13genes/Blast-vs-draft-hybrid-genome.sh
@@ -1,10 +1,10 @@
 assembly=~/Documents/stage_mbelari/results/hybrid_test/DBG2OLC/2018-06-01/final_assembly.fasta
-fasta_file=~/Documents/stage_mbelari/results/13genes/version2/sex-determination-genes.fasta
+fasta_file=~/Documents/stage_mbelari/results/annotation/coverage_analysis/2018-07-24-Genes_absent_in_females.fasta
 
-outputfile=2018-07-04-sex-deter-genes.txt
-outputfilealign=2018-07-sex-deter-genes.txt
+outputfile=2018-07-24-BLAST_Genes_absent_in_females.txt
+outputfilealign=2018-07-24-BLAST_Genes_absent_in_females.align.txt
 
 #makeblastdb -in ${assembly} -parse_seqids -dbtype nucl
 
 blastn -query ${fasta_file} -db ${assembly} -out ${outputfile} -outfmt 6
-blastn -query ${fasta_file} -db ${assembly} -out ${outputfilealign}
+blastn -query ${fasta_file} -db ${assembly} -out ${outputfilealign} -outfmt 4
