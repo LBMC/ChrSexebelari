128780ce1bf7fcf8297f388dc5c584b3b2ffcebf
Lilia <lilia.estrada-virrueta@ens-lyon.fr>
2018-07-27 17:33:07 +0200

    new running

--- src/coverage_analysis/get_counts_per_blast_hit.sh
+++ src/coverage_analysis/get_counts_per_blast_hit.sh
@@ -11,12 +11,11 @@
 #bash src/date.sh results/mapping/without_duplicates/MRDR6_trim_Mbelari_mapped_rmdup_rg_realign_indels_bam_to_bed.bed &&\
 
 #Sort bed
-#-faidx ~/Documents/stage_mbelari/results/coverage_analysis/2018-06-21-Mbelari_hybrid_genome_sizes.txt
 
-#bedtools sort -faidx ~/Documents/stage_mbelari/results/coverage_analysis/2018-06-21-Mbelari_hybrid_genome_sizes.txt -i ~/Documents/stage_mbelari/results/annotation/coverage_analysis/2018-07-24-Blast_female_absent_genes_hits.bed > ~/Documents/stage_mbelari/results/annotation/coverage_analysis/2018-07-24-Blast_female_absent_genes_hits.sorted.bed
+bedtools sort -faidx ~/Documents/stage_mbelari/results/coverage_analysis/2018-06-21-Mbelari_hybrid_genome_sizes.txt -i ~/Documents/stage_mbelari/results/duplications/2018-07-26-Blast_female_absent_genes_hits.bed > ~/Documents/stage_mbelari/results/duplications/2018-07-26-Blast_female_absent_genes_hits.sorted.bed
 
 # Make count of nb of reads falling within genes -g ~/Documents/stage_mbelari/results/coverage_analysis/2018-06-21-Mbelari_hybrid_genome_sizes.txt
-bedtools map -o count -g ~/Documents/stage_mbelari/results/coverage_analysis/2018-06-21-Mbelari_hybrid_genome_sizes.txt -a ~/Documents/stage_mbelari/results/annotation/coverage_analysis/2018-07-24-Blast_female_absent_genes_hits.sorted.bed -b ~/Documents/stage_mbelari/results/coverage_analysis/2018-06-12-MRDR5_vs_Hybrid_assembly.sorted.filtered.bam_to_bed.bed > ~/Documents/stage_mbelari/results/annotation/coverage_analysis/2018-07-24-Blast_female_absent_genes_hits_MRDR5_counts.txt &
+bedtools map -o count -g ~/Documents/stage_mbelari/results/coverage_analysis/2018-06-21-Mbelari_hybrid_genome_sizes.txt -a ~/Documents/stage_mbelari/results/duplications/2018-07-26-Blast_female_absent_genes_hits.sorted.bed -b ~/Documents/stage_mbelari/results/coverage_analysis/2018-06-12-MRDR5_vs_Hybrid_assembly.sorted.filtered.bam_to_bed.bed > ~/Documents/stage_mbelari/results/duplications/2018-07-26-Blast_female_absent_genes_hits_MRDR5_counts.txt &
 
-bedtools map -o count -g ~/Documents/stage_mbelari/results/coverage_analysis/2018-06-21-Mbelari_hybrid_genome_sizes.txt -a ~/Documents/stage_mbelari/results/annotation/coverage_analysis/2018-07-24-Blast_female_absent_genes_hits.sorted.bed -b ~/Documents/stage_mbelari/results/coverage_analysis/2018-06-12-MRDR6_vs_Hybrid_assembly.sorted.filtered.bam_to_bed.bed > ~/Documents/stage_mbelari/results/annotation/coverage_analysis/2018-07-24-Blast_female_absent_genes_hits_MRDR6_counts.txt &
+bedtools map -o count -g ~/Documents/stage_mbelari/results/coverage_analysis/2018-06-21-Mbelari_hybrid_genome_sizes.txt -a ~/Documents/stage_mbelari/results/duplications/2018-07-26-Blast_female_absent_genes_hits.sorted.bed -b ~/Documents/stage_mbelari/results/coverage_analysis/2018-06-12-MRDR6_vs_Hybrid_assembly.sorted.filtered.bam_to_bed.bed > ~/Documents/stage_mbelari/results/duplications/2018-07-26-Blast_female_absent_genes_hits_MRDR6_counts.txt &
 
