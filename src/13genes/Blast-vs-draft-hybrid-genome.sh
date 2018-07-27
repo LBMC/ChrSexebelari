12c6ad312973f91cf3d0d2d5b7c68daed70dbf88
Lilia <lilia.estrada-virrueta@ens-lyon.fr>
2018-07-27 17:32:02 +0200

    new blast

--- src/13genes/Blast-vs-draft-hybrid-genome.sh
+++ src/13genes/Blast-vs-draft-hybrid-genome.sh
@@ -1,8 +1,8 @@
 assembly=~/Documents/stage_mbelari/results/hybrid_test/DBG2OLC/2018-06-01/final_assembly.fasta
-fasta_file=~/Documents/stage_mbelari/results/annotation/coverage_analysis/2018-07-24-Genes_absent_in_females.fasta
+fasta_file=~/Documents/stage_mbelari/data/raw/RNAP2.fasta
 
-outputfile=2018-07-24-BLAST_Genes_absent_in_females.txt
-outputfilealign=2018-07-24-BLAST_Genes_absent_in_females.align.txt
+outputfile=2018-07-27-RNAP2.txt
+outputfilealign=2018-07-27-RNAP2.align.txt
 
 #makeblastdb -in ${assembly} -parse_seqids -dbtype nucl
 
