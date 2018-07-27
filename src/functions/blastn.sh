2b952caabfa5841cf4fcb8fd829e37e24cada407
Lilia <lilia.estrada-virrueta@ens-lyon.fr>
2018-07-27 17:42:51 +0200

    run with new fasta file

--- src/functions/blastn.sh
+++ src/functions/blastn.sh
@@ -1,8 +1,8 @@
 assembly=~/Documents/stage_mbelari/results/hybrid_test/DBG2OLC/2018-06-01/final_assembly.fasta
-fasta_file=~/Documents/stage_mbelari/results/annotation/coverage_analysis/2018-07-24-Genes_absent_in_females.fasta
+fasta_file=~/Documents/stage_mbelari/results/duplications/2018-07-26-complete_seq_genes_absent_females.fasta
 
-outputfile=2018-07-24-BLAST_Genes_absent_in_females.txt
-outputfilealign=2018-07-24-BLAST_Genes_absent_in_females.align.txt
+outputfile=2018-07-26-BLAST_Genes_absent_in_females_genomic_seq.txt
+outputfilealign=2018-07-26-BLAST_Genes_absent_in_females_genomic_seq.align.txt
 
 #makeblastdb -in ${assembly} -parse_seqids -dbtype nucl
 
