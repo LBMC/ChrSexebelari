253c0bf907ec083d35f69b60a8547210fa0ebea0
Laurent Modolo <laurent@modolo.fr>
2018-11-14 16:52:59 +0100

    intersect_bed_genes.sh: fix path

--- src/5-coverage_analysis/intersect_bed_genes.sh
+++ src/5-coverage_analysis/intersect_bed_genes.sh
@@ -9,7 +9,7 @@
 #bedtools intersect -a tmp_input_intersect_d_genes.bed -b results/annotation/coverage_analysis/Mesorhabditis_belari_JU2817_hybrid_assembly_genes_1based.bed -wb > results/annotation/coverage_analysis/2018-07-16-MRDR6_vs_Hybrid_assembly_sort_in_genes.bed
 
 awk 'OFS="\t" { print $1, $2, $2, $3 }' results/coverage_analysis/2018_07_25_MRDR5_vs_Hybrid_assembly.sorted.filtered.bed > tmp_input_intersect_d_genes.bed
-bedtools intersect -a tmp_input_intersect_d_genes.bed -b results/annotation/coverage_analysis/Mesorhabditis_belari_JU2817_hybrid_assembly_genes_1based.bed -wb > results/annotation/coverage_analysis/2018-07-25-MRDR5_vs_Hybrid_assembly_sort_in_genes_bp.bed
+bedtools intersect -a tmp_input_intersect_d_genes.bed -b results/coverage_analysis/Mesorhabditis_belari_JU2817_hybrid_assembly_genes_1based.bed -wb > results/coverage_analysis/2018-07-25-MRDR5_vs_Hybrid_assembly_sort_in_genes_bp.bed
 
 awk 'OFS="\t" { print $1, $2, $2, $3 }' results/coverage_analysis/2018_07_25_MRDR6_vs_Hybrid_assembly.sorted.filtered.bed > tmp_input_intersect_d_genes.bed
-bedtools intersect -a tmp_input_intersect_d_genes.bed -b results/annotation/coverage_analysis/Mesorhabditis_belari_JU2817_hybrid_assembly_genes_1based.bed -wb > results/annotation/coverage_analysis/2018-07-25-MRDR6_vs_Hybrid_assembly_sort_in_genes_bp.bed
+bedtools intersect -a tmp_input_intersect_d_genes.bed -b results/coverage_analysis/Mesorhabditis_belari_JU2817_hybrid_assembly_genes_1based.bed -wb > results/coverage_analysis/2018-07-25-MRDR6_vs_Hybrid_assembly_sort_in_genes_bp.bed
