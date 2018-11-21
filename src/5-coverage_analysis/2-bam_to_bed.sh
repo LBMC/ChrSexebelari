eb439e63c1a0ca27db2aaa914c941d7a6f85ff16
Laurent Modolo <laurent@modolo.fr>
2018-11-21 11:44:42 +0100

    2-bam_to_bed.R: add cmd for F and merge pb cov F & M

--- src/5-coverage_analysis/2-bam_to_bed.sh
+++ src/5-coverage_analysis/2-bam_to_bed.sh
@@ -1,8 +1,18 @@
 #The bam files were filtered by quality
 
 INPUTcov=results/coverage_analysis/2018-06-12-MRDR6_vs_Hybrid_assembly.sorted.filtered.bam
-GENOMEsizes=results/coverage_analysis/2018-06-21-Mbelari_hybrid_genome_sizes.txt
 OUTPUTcov=results/coverage_analysis/2018_07_25_MRDR6_vs_Hybrid_assembly.sorted.filtered.bed
 
-bedtools genomecov -d -ibam $INPUTcov -g $GENOMEsizes > $OUTPUTcov
+bedtools genomecov -d -ibam $INPUTcov > $OUTPUTcov
 
+INPUTcov=results/coverage_analysis/2018-06-12-MRDR5_vs_Hybrid_assembly.sorted.filtered.bam
+OUTPUTcov=results/coverage_analysis/2018_07_25_MRDR5_vs_Hybrid_assembly.sorted.filtered.bed
+
+bedtools genomecov -d -ibam $INPUTcov > $OUTPUTcov
+
+
+BED1=results/coverage_analysis/2018_07_25_MRDR6_vs_Hybrid_assembly.sorted.filtered.bed
+BED2=results/coverage_analysis/2018_07_25_MRDR5_vs_Hybrid_assembly.sorted.filtered.bed
+OUTPUTcov=results/coverage_analysis/2018_07_25_MRDR5_MRDR6_vs_Hybrid_assembly.sorted.filtered.bed
+
+paste $BED1 $BED2 > $OUTPUTcov
